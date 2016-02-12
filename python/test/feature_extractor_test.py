d70a5ef3135914421616684c7294c27cd03420c0
Zhi Li <zli@netflix.com>
2016-02-12 12:55:43 -0800

    Refactor: move QualityRunner some functions to Executor.

--- python/test/feature_extractor_test.py
+++ python/test/feature_extractor_test.py
@@ -3,9 +3,15 @@ __license__ = "Apache, Version 2.0"
 
 import unittest
 from feature_extractor import VmafFeatureExtractor
+from asset import Asset
 
 class FeatureExtractorTest(unittest.TestCase):
 
     def test_executor_id(self):
-        fextractor = VmafFeatureExtractor()
+        asset = Asset(dataset="test", content_id=0, asset_id=1,
+                      ref_path="dir/refvideo.yuv", dis_path="dir/disvideo.yuv",
+                      asset_dict={})
+        fextractor = VmafFeatureExtractor([asset], None,
+                                   log_file_dir="log_file_dir",
+                                   fifo_mode=True)
         self.assertEquals(fextractor.executor_id, "VMAF_feature_V0.1")
