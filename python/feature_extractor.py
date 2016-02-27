e4b2b8f96d188f75c3183392df604362cfe029f8
Zhi Li <zli@netflix.com>
2016-02-27 00:41:44 -0800

    Misc.

--- python/feature_extractor.py
+++ python/feature_extractor.py
@@ -35,6 +35,80 @@ class FeatureExtractor(Executor):
         return "{type}_{atom_feature}_scores".format(
             type=cls.TYPE, atom_feature=atom_feature)
 
+
+class VmafFeatureExtractor(FeatureExtractor):
+
+    TYPE = "VMAF_feature"
+    VERSION = '0.1'
+
+    ATOM_FEATURES = ['vif', 'adm', 'ansnr', 'motion']
+
+    VMAF_FEATURE = config.ROOT + "/feature/vmaf"
+
+    def _run_and_generate_log_file(self, asset):
+        # routine to call the command-line executable and generate feature
+        # scores in the log file.
+
+        log_file_path = self._get_log_file_path(asset)
+
+        # run VMAF command line to extract features, APPEND (>>) result (since
+        # _prepare_generate_log_file method has already created the file and
+        # written something in advance).
+        quality_width, quality_height = asset.quality_width_height
+        vmaf_feature_cmd = "{vmaf} all {yuv_type} {ref_path} {dis_path} {w} {h} >> {log_file_path}" \
+        .format(
+            vmaf=self.VMAF_FEATURE,
+            yuv_type=asset.yuv_type,
+            ref_path=asset.ref_workfile_path,
+            dis_path=asset.dis_workfile_path,
+            w=quality_width,
+            h=quality_height,
+            log_file_path=log_file_path,
+        )
+
+        if self.logger:
+            self.logger.info(vmaf_feature_cmd)
+
+        subprocess.call(vmaf_feature_cmd, shell=True)
+
+    def _get_feature_scores(self, asset):
+        # routine to read the feature scores from the log file, and return
+        # the scores in a dictionary format.
+
+        log_file_path = self._get_log_file_path(asset)
+
+        atom_feature_scores_dict = {}
+        atom_feature_idx_dict = {}
+        for atom_feature in self.ATOM_FEATURES:
+            atom_feature_scores_dict[atom_feature] = []
+            atom_feature_idx_dict[atom_feature] = 0
+
+        with open(log_file_path, 'rt') as log_file:
+            for line in log_file.readlines():
+                for atom_feature in self.ATOM_FEATURES:
+                    re_template = "{af}: ([0-9]+) ([0-9.-]+)".format(af=atom_feature)
+                    mo = re.match(re_template, line)
+                    if mo:
+                        cur_idx = int(mo.group(1))
+                        assert cur_idx == atom_feature_idx_dict[atom_feature]
+                        atom_feature_scores_dict[atom_feature].append(float(mo.group(2)))
+                        atom_feature_idx_dict[atom_feature] += 1
+                        continue
+
+        len_score = len(atom_feature_scores_dict[self.ATOM_FEATURES[0]])
+        assert len_score != 0
+        for atom_feature in self.ATOM_FEATURES[1:]:
+            assert len_score == len(atom_feature_scores_dict[atom_feature])
+
+        feature_result = {}
+
+        for atom_feature in self.ATOM_FEATURES:
+            scores_key = self.get_scores_key(atom_feature)
+            feature_result[scores_key] = atom_feature_scores_dict[atom_feature]
+
+        return feature_result
+
+
 class MomentFeatureExtractor(FeatureExtractor):
 
     TYPE = "Moment_feature"
@@ -130,79 +204,6 @@ class MomentFeatureExtractor(FeatureExtractor):
                     atom_feature_scores_dict[atom_feature].append(float(mo.group(2)))
                     atom_feature_idx_dict[atom_feature] += 1
                     continue
-
-        len_score = len(atom_feature_scores_dict[self.ATOM_FEATURES[0]])
-        assert len_score != 0
-        for atom_feature in self.ATOM_FEATURES[1:]:
-            assert len_score == len(atom_feature_scores_dict[atom_feature])
-
-        feature_result = {}
-
-        for atom_feature in self.ATOM_FEATURES:
-            scores_key = self.get_scores_key(atom_feature)
-            feature_result[scores_key] = atom_feature_scores_dict[atom_feature]
-
-        return feature_result
-
-
-class VmafFeatureExtractor(FeatureExtractor):
-
-    TYPE = "VMAF_feature"
-    VERSION = '0.1'
-
-    ATOM_FEATURES = ['vif', 'adm', 'ansnr', 'motion']
-
-    VMAF_FEATURE = config.ROOT + "/feature/vmaf"
-
-    def _run_and_generate_log_file(self, asset):
-        # routine to call the command-line executable and generate feature
-        # scores in the log file.
-
-        log_file_path = self._get_log_file_path(asset)
-
-        # run VMAF command line to extract features, APPEND (>>) result (since
-        # _prepare_generate_log_file method has already created the file and
-        # written something in advance).
-        quality_width, quality_height = asset.quality_width_height
-        vmaf_feature_cmd = "{vmaf} all {yuv_type} {ref_path} {dis_path} {w} {h} >> {log_file_path}" \
-        .format(
-            vmaf=self.VMAF_FEATURE,
-            yuv_type=asset.yuv_type,
-            ref_path=asset.ref_workfile_path,
-            dis_path=asset.dis_workfile_path,
-            w=quality_width,
-            h=quality_height,
-            log_file_path=log_file_path,
-        )
-
-        if self.logger:
-            self.logger.info(vmaf_feature_cmd)
-
-        subprocess.call(vmaf_feature_cmd, shell=True)
-
-    def _get_feature_scores(self, asset):
-        # routine to read the feature scores from the log file, and return
-        # the scores in a dictionary format.
-
-        log_file_path = self._get_log_file_path(asset)
-
-        atom_feature_scores_dict = {}
-        atom_feature_idx_dict = {}
-        for atom_feature in self.ATOM_FEATURES:
-            atom_feature_scores_dict[atom_feature] = []
-            atom_feature_idx_dict[atom_feature] = 0
-
-        with open(log_file_path, 'rt') as log_file:
-            for line in log_file.readlines():
-                for atom_feature in self.ATOM_FEATURES:
-                    re_template = "{af}: ([0-9]+) ([0-9.-]+)".format(af=atom_feature)
-                    mo = re.match(re_template, line)
-                    if mo:
-                        cur_idx = int(mo.group(1))
-                        assert cur_idx == atom_feature_idx_dict[atom_feature]
-                        atom_feature_scores_dict[atom_feature].append(float(mo.group(2)))
-                        atom_feature_idx_dict[atom_feature] += 1
-                        continue
 
         len_score = len(atom_feature_scores_dict[self.ATOM_FEATURES[0]])
         assert len_score != 0
---
+++ resource/feature_param/vmaf_feature_v2.py
@@ -0,0 +1,6 @@
+feature_dict = {
+
+    'VMAF_feature':'all', # use all features from VmafFeatureExtractor
+
+    'Moment_feature':['dis1st'], # use dis video's 1st moment
+}
