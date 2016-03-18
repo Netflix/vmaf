052949861b5a682490a0f109ce35dd49b3e0802c
Zhi Li <zli@netflix.com>
2016-03-18 13:34:07 -0700

    Add YuvReader and tests.

--- feature/test/feature_test.py
+++ feature/test/feature_test.py
@@ -22,7 +22,7 @@ def read_log(log_filename, type):
     score = sum(scores) / float(len(scores))
     return score, scores
 
-class SingleFeatureTest(unittest.TestCase):
+class FeatureTest(unittest.TestCase):
 
     VMAF = config.ROOT + "/feature/vmaf"
     PSNR = config.ROOT + "/feature/psnr"
@@ -84,21 +84,6 @@ class SingleFeatureTest(unittest.TestCase):
         self.assertAlmostEquals(scores[0], 0.574283)
         self.assertAlmostEquals(scores[1], 0.491295)
 
-    def test_motion_yuv420p10le(self):
-        print 'test motion on yuv420p10le format...'
-        cmd = "{vmaf} motion yuv420p10le {ref} {dis} {w} {h} > {log}".format(
-            vmaf=self.VMAF, ref=self.REF_YUV, dis=self.DIS_YUV,
-            w=self.YUV_WIDTH, h=self.YUV_HEIGHT, log=self.LOG_FILENAME
-        )
-        subprocess.call(cmd, shell=True)
-        score, scores = read_log(self.LOG_FILENAME, "motion")
-        self.assertAlmostEquals(score, 263.27415975)
-        self.assertAlmostEquals(scores[0], 0.0)
-        self.assertAlmostEquals(scores[1], 281.234406)
-        self.assertAlmostEquals(scores[23], 324.084839)
-        # 28 frames yuv420p should be 24 frames in yuv420p10le
-        self.assertAlmostEquals(len(scores), 24)
-
     def test_all(self):
         print 'test all...'
         cmd = "{vmaf} all {fmt} {ref} {dis} {w} {h} > {log}".format(
@@ -138,6 +123,109 @@ class SingleFeatureTest(unittest.TestCase):
         self.assertAlmostEquals(score, 61.332006624999984)
         score, scores = read_log(self.LOG_FILENAME, "2ndmoment")
         self.assertAlmostEquals(score, 4798.659574041666)
+
+class FeatureTestYuv422p10le(unittest.TestCase):
+
+    VMAF = config.ROOT + "/feature/vmaf"
+    PSNR = config.ROOT + "/feature/psnr"
+    MOMENT = config.ROOT + "/feature/moment"
+    LOG_FILENAME = config.ROOT + "/workspace/log"
+    REF_YUV = config.ROOT + "/resource/yuv/src01_hrc00_576x324.yuv422p10le.yuv"
+    DIS_YUV = config.ROOT + "/resource/yuv/src01_hrc01_576x324.yuv422p10le.yuv"
+    YUV_FMT = "yuv422p10le"
+    YUV_WIDTH = 576
+    YUV_HEIGHT = 324
+
+    def setUp(self):
+        if os.path.exists(self.LOG_FILENAME):
+            os.remove(self.LOG_FILENAME)
+
+    def tearDown(self):
+        if os.path.exists(self.LOG_FILENAME):
+            os.remove(self.LOG_FILENAME)
+
+    def test_adm(self):
+        print 'test adm on yuv422p10le...'
+        cmd = "{vmaf} adm {fmt} {ref} {dis} {w} {h} > {log}".format(
+            vmaf=self.VMAF, fmt=self.YUV_FMT, ref=self.REF_YUV, dis=self.DIS_YUV,
+            w=self.YUV_WIDTH, h=self.YUV_HEIGHT, log=self.LOG_FILENAME
+        )
+        subprocess.call(cmd, shell=True)
+        score, scores = read_log(self.LOG_FILENAME, "adm")
+        self.assertAlmostEquals(score, 0.9155242291666666)
+
+    def test_ansnr(self):
+        print 'test ansnr on yuv422p10le...'
+        cmd = "{vmaf} ansnr {fmt} {ref} {dis} {w} {h} > {log}".format(
+            vmaf=self.VMAF, fmt=self.YUV_FMT, ref=self.REF_YUV, dis=self.DIS_YUV,
+            w=self.YUV_WIDTH, h=self.YUV_HEIGHT, log=self.LOG_FILENAME
+        )
+        subprocess.call(cmd, shell=True)
+        score, scores = read_log(self.LOG_FILENAME, "ansnr")
+        self.assertAlmostEquals(score, 22.53345677083333)
+
+    def test_motion(self):
+        print 'test motion on yuv422p10le...'
+        cmd = "{vmaf} motion {fmt} {ref} {dis} {w} {h} > {log}".format(
+            vmaf=self.VMAF, fmt=self.YUV_FMT, ref=self.REF_YUV, dis=self.DIS_YUV,
+            w=self.YUV_WIDTH, h=self.YUV_HEIGHT, log=self.LOG_FILENAME
+        )
+        subprocess.call(cmd, shell=True)
+        score, scores = read_log(self.LOG_FILENAME, "motion")
+        self.assertAlmostEquals(score, 3.5916076041666667)
+
+    def test_vif(self):
+        print 'test vif on yuv422p10le...'
+        cmd = "{vmaf} vif {fmt} {ref} {dis} {w} {h} > {log}".format(
+            vmaf=self.VMAF, fmt=self.YUV_FMT, ref=self.REF_YUV, dis=self.DIS_YUV,
+            w=self.YUV_WIDTH, h=self.YUV_HEIGHT, log=self.LOG_FILENAME
+        )
+        subprocess.call(cmd, shell=True)
+        score, scores = read_log(self.LOG_FILENAME, "vif")
+        self.assertAlmostEquals(score, 0.44417014583333336)
+        self.assertAlmostEquals(scores[0], 0.574283)
+        self.assertAlmostEquals(scores[1], 0.491295)
+
+    def test_all(self):
+        print 'test all on yuv422p10le...'
+        cmd = "{vmaf} all {fmt} {ref} {dis} {w} {h} > {log}".format(
+            vmaf=self.VMAF, fmt=self.YUV_FMT, ref=self.REF_YUV, dis=self.DIS_YUV,
+            w=self.YUV_WIDTH, h=self.YUV_HEIGHT, log=self.LOG_FILENAME
+        )
+        subprocess.call(cmd, shell=True)
+        score, scores = read_log(self.LOG_FILENAME, "vif")
+        self.assertAlmostEquals(score, 0.44417014583333336)
+        score, scores = read_log(self.LOG_FILENAME, "motion")
+        self.assertAlmostEquals(score, 3.5916076041666667)
+        score, scores = read_log(self.LOG_FILENAME, "ansnr")
+        self.assertAlmostEquals(score, 22.53345677083333)
+        score, scores = read_log(self.LOG_FILENAME, "adm")
+        self.assertAlmostEquals(score, 0.9155242291666666)
+
+    def test_psnr(self):
+        print 'test psnr on yuv422p10le...'
+        cmd = "{psnr} {fmt} {ref} {dis} {w} {h} > {log}".format(
+            psnr=self.PSNR, fmt=self.YUV_FMT, ref=self.REF_YUV, dis=self.DIS_YUV,
+            w=self.YUV_WIDTH, h=self.YUV_HEIGHT, log=self.LOG_FILENAME
+        )
+        subprocess.call(cmd, shell=True)
+        score, scores = read_log(self.LOG_FILENAME, "psnr")
+        self.assertAlmostEquals(score, 30.78057329166666)
+        self.assertAlmostEquals(scores[0], 34.786288)
+        self.assertAlmostEquals(scores[1], 31.908737)
+
+    def test_2nd_moment(self):
+        print 'test 2nd moment on yuv422p10le...'
+        cmd = "{moment} 2 {fmt} {dis} {w} {h} > {log}".format(
+            moment=self.MOMENT, fmt=self.YUV_FMT, dis=self.DIS_YUV,
+            w=self.YUV_WIDTH, h=self.YUV_HEIGHT, log=self.LOG_FILENAME
+        )
+        subprocess.call(cmd, shell=True)
+        score, scores = read_log(self.LOG_FILENAME, "1stmoment")
+        self.assertAlmostEquals(score, 61.332006624999984)
+        score, scores = read_log(self.LOG_FILENAME, "2ndmoment")
+        self.assertAlmostEquals(score, 4798.659574041666)
+
 
 class CornerCaseTest(unittest.TestCase):
 
--- python/test/reader_test.py
+++ python/test/reader_test.py
@@ -3,6 +3,8 @@ __license__ = "Apache, Version 2.0"
 
 import unittest
 
+import numpy as np
+
 import config
 from tools.reader import YuvReader
 
@@ -17,3 +19,151 @@ class YuvReaderTest(unittest.TestCase):
         )
         self.assertEquals(yuv_reader.num_bytes, 13436928)
         self.assertEquals(yuv_reader.num_frms, 48)
+        self.assertEquals(yuv_reader._get_uv_width_height_multiplier(), (0.5, 0.5))
+
+    def test_with(self):
+        with YuvReader(
+            filepath=config.ROOT + "/resource/yuv/src01_hrc00_576x324.yuv",
+            width=576,
+            height=324,
+            yuv_type='yuv420p'
+        ) as yuv_reader:
+            self.assertEquals(yuv_reader.file.__class__, file)
+
+    def test_next_y_u_v(self):
+        with YuvReader(
+            filepath=config.ROOT + "/resource/yuv/src01_hrc00_576x324.yuv",
+            width=576,
+            height=324,
+            yuv_type='yuv420p'
+        ) as yuv_reader:
+
+            y, u, v = yuv_reader.next_y_u_v()
+
+            self.assertEquals(y[0][0], 87)
+            self.assertEquals(y[0][1], 131)
+            self.assertEquals(y[1][0], 95)
+
+            self.assertEquals(u[0][0], 92)
+            self.assertEquals(u[0][1], 97)
+            self.assertEquals(u[1][0], 90)
+
+            self.assertEquals(v[0][0], 121)
+            self.assertEquals(v[0][1], 126)
+            self.assertEquals(v[1][0], 122)
+
+            self.assertAlmostEquals(y.mean(), 61.928749785665296)
+            self.assertAlmostEquals(u.mean(), 114.6326517489712)
+            self.assertAlmostEquals(v.mean(), 122.05084019204389)
+
+            y, u, v = yuv_reader.next_y_u_v()
+
+            self.assertEquals(y[0][0], 142)
+            self.assertEquals(y[0][1], 128)
+            self.assertEquals(y[1][0], 134)
+
+            self.assertEquals(u[0][0], 93)
+            self.assertEquals(u[0][1], 102)
+            self.assertEquals(u[1][0], 91)
+
+            self.assertEquals(v[0][0], 128)
+            self.assertEquals(v[0][1], 126)
+            self.assertEquals(v[1][0], 124)
+
+            self.assertAlmostEquals(y.mean(), 61.265260631001375)
+            self.assertAlmostEquals(u.mean(), 114.72515860768175)
+            self.assertAlmostEquals(v.mean(), 122.12022033607681)
+
+    def test_iteration(self):
+
+        y_1stmoments = []
+        y_2ndmoments = []
+
+        with YuvReader(
+                filepath=config.ROOT + "/resource/yuv/src01_hrc01_576x324.yuv",
+                width=576, height=324, yuv_type='yuv420p') as yuv_reader:
+
+            for y, u, v in yuv_reader:
+                y_1stmoments.append(y.mean())
+                y_2ndmoments.append(y.var() + y.mean() * y.mean())
+
+        self.assertEquals(len(y_1stmoments), 48)
+        self.assertEquals(len(y_2ndmoments), 48)
+        self.assertAlmostEquals(np.mean(y_1stmoments), 61.332006624999984)
+        self.assertAlmostEquals(np.mean(y_2ndmoments), 4798.659574041666)
+
+class YuvReaderTest10le(unittest.TestCase):
+
+    def test_yuv_reader(self):
+        yuv_reader = YuvReader(
+            filepath=config.ROOT + "/resource/yuv/src01_hrc00_576x324.yuv422p10le.yuv",
+            width=576,
+            height=324,
+            yuv_type='yuv422p10le'
+        )
+        self.assertEquals(yuv_reader.num_bytes, 35831808)
+        self.assertEquals(yuv_reader.num_frms, 48)
+        self.assertEquals(yuv_reader._get_uv_width_height_multiplier(), (0.5, 1.0))
+
+    def test_with(self):
+        with YuvReader(
+            filepath=config.ROOT + "/resource/yuv/src01_hrc00_576x324.yuv422p10le.yuv",
+            width=576,
+            height=324,
+            yuv_type='yuv422p10le'
+        ) as yuv_reader:
+
+            y, u, v = yuv_reader.next_y_u_v()
+
+            self.assertEquals(y[0][0], 87)
+            self.assertEquals(y[0][1], 131)
+            self.assertEquals(y[1][0], 95)
+
+            self.assertEquals(u[0][0], 92.25)
+            self.assertEquals(u[0][1], 97.5)
+            self.assertEquals(u[1][0], 91.75)
+
+            self.assertEquals(v[0][0], 121)
+            self.assertEquals(v[0][1], 126.25)
+            self.assertEquals(v[1][0], 121.25)
+
+            self.assertAlmostEquals(y.mean(), 61.928749785665296)
+            self.assertAlmostEquals(u.mean(), 114.63283661265432)
+            self.assertAlmostEquals(v.mean(), 122.05113490226337)
+
+            y, u, v = yuv_reader.next_y_u_v()
+
+            self.assertEquals(y[0][0], 142)
+            self.assertEquals(y[0][1], 128)
+            self.assertEquals(y[1][0], 134)
+
+            self.assertEquals(u[0][0], 93.25)
+            self.assertEquals(u[0][1], 102.75)
+            self.assertEquals(u[1][0], 92.75)
+
+            self.assertEquals(v[0][0], 128.25)
+            self.assertEquals(v[0][1], 126.5)
+            self.assertEquals(v[1][0], 127.25)
+
+            self.assertAlmostEquals(y.mean(), 61.265260631001375)
+            self.assertAlmostEquals(u.mean(), 114.72527917095336)
+            self.assertAlmostEquals(v.mean(), 122.12047217935527)
+
+    def test_iteration(self):
+
+        y_1stmoments = []
+        y_2ndmoments = []
+
+        with YuvReader(
+                filepath=config.ROOT +
+                        "/resource/yuv/src01_hrc01_576x324.yuv422p10le.yuv",
+                width=576, height=324, yuv_type='yuv422p10le') as yuv_reader:
+
+            for y, u, v in yuv_reader:
+                y_1stmoments.append(y.mean())
+                y_2ndmoments.append(y.var() + y.mean() * y.mean())
+
+        self.assertEquals(len(y_1stmoments), 48)
+        self.assertEquals(len(y_2ndmoments), 48)
+        self.assertAlmostEquals(np.mean(y_1stmoments), 61.332006624999984)
+        self.assertAlmostEquals(np.mean(y_2ndmoments), 4798.659574041666)
