6d39cdcad0871014970d0a50635d539ae88555b6
Zhi Li <zli@netflix.com>
2016-04-10 11:50:11 -0700

    Remove train_on_dataset.

--- python/test/routine_test.py
+++ python/test/routine_test.py
@@ -5,7 +5,7 @@ import os
 import unittest
 
 import config
-from routine import train_on_dataset, train_test_on_dataset
+from routine import train_test_on_dataset
 from tools.misc import import_python_file
 
 
@@ -22,29 +22,6 @@ class TestTrainOnDataset(unittest.TestCase):
             self.train_fassembler.remove_logs()
         if os.path.exists(self.output_model_filepath):
             os.remove(self.output_model_filepath)
-
-    @unittest.skip("train_on_dataset is deprecated")
-    def test_train_on_dataset_with_dis1st_thr(self):
-        feature_param = import_python_file(
-            config.ROOT + '/python/test/resource/feature_param_sample.py')
-        model_param = import_python_file(
-            config.ROOT + '/python/teset/resource/model_param_sample.py')
-        train_fassembler, train_stats = \
-            train_on_dataset(train_dataset=self.train_dataset,
-                         feature_param=feature_param,
-                         model_param=model_param,
-                         ax=None,
-                         result_store=None,
-                         output_model_filepath=self.output_model_filepath,
-                         parallelize=True,
-                         fifo_mode=True,
-                         logger=None,
-                         )
-        self.train_fassembler = train_fassembler
-        self.assertTrue(os.path.exists(self.output_model_filepath))
-        self.assertItemsEqual(train_stats['ys_label_pred'],
-                                [91.707522376672316, 58.277822562766268,
-                                 95.577344864265129, 90.255945341860439])
 
     def test_train_test_on_dataset_with_dis1st_thr(self):
         feature_param = import_python_file(
