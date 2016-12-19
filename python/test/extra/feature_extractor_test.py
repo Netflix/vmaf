15db1f0e1fe709195a0fe98a13a9fd2df33ee264
Zhi Li <zli@netflix.com>
2016-12-19 07:17:53 -0800

    Add cropping test case.

--- python/test/extra/feature_extractor_test.py
+++ python/test/extra/feature_extractor_test.py
@@ -31,7 +31,7 @@ class ParallelFeatureExtractorTestNew(unittest.TestCase):
                                   'quality_width':160, 'quality_height':90})
 
         self.fextractor = VmafFeatureExtractor(
-            [asset, asset_original], None, fifo_mode=True)
+            [asset, asset_original], None, fifo_mode=False)
 
         self.fextractor.run(parallelize=False)
 
@@ -46,6 +46,40 @@ class ParallelFeatureExtractorTestNew(unittest.TestCase):
         self.assertAlmostEqual(results[1]['VMAF_feature_motion_score'], 1.3216766875, places=4)
         self.assertAlmostEqual(results[1]['VMAF_feature_adm_score'], 1.0, places=4)
         self.assertAlmostEqual(results[1]['VMAF_feature_ansnr_score'], 31.811350125, places=4)
+
+    def test_run_vmaf_fextractor_with_cropping(self):
+        ref_path = config.ROOT + "/resource/yuv/src01_hrc00_576x324.yuv"
+        dis_path = config.ROOT + "/resource/yuv/src01_hrc01_576x324.yuv"
+        asset = Asset(dataset="test", content_id=0, asset_id=1,
+                      workdir_root=config.ROOT + "/workspace/workdir",
+                      ref_path=ref_path,
+                      dis_path=dis_path,
+                      asset_dict={'width':576, 'height':324,
+                                  'crop_cmd':'570:320:3:2'})
+
+        asset_original = Asset(dataset="test", content_id=0, asset_id=2,
+                      workdir_root=config.ROOT + "/workspace/workdir",
+                      ref_path=ref_path,
+                      dis_path=ref_path,
+                      asset_dict={'width':576, 'height':324,
+                                  'crop_cmd':'570:320:3:2'})
+
+        self.fextractor = VmafFeatureExtractor(
+            [asset, asset_original], None, fifo_mode=False)
+
+        self.fextractor.run(parallelize=False)
+
+        results = self.fextractor.results
+
+        self.assertAlmostEqual(results[0]['VMAF_feature_vif_score'], 0.4460930625, places=4)
+        self.assertAlmostEqual(results[0]['VMAF_feature_motion_score'], 4.04982535417, places=4)
+        self.assertAlmostEqual(results[0]['VMAF_feature_adm2_score'],0.925421075027, places=4)
+        self.assertAlmostEqual(results[0]['VMAF_feature_ansnr_score'], 23.5095715208, places=4)
+
+        self.assertAlmostEqual(results[1]['VMAF_feature_vif_score'], 1.0, places=4)
+        self.assertAlmostEqual(results[1]['VMAF_feature_motion_score'], 4.04982535417, places=4)
+        self.assertAlmostEqual(results[1]['VMAF_feature_adm2_score'], 1.0, places=4)
+        self.assertAlmostEqual(results[1]['VMAF_feature_ansnr_score'], 31.2714392708, places=4)
 
 if __name__ == '__main__':
     unittest.main()
