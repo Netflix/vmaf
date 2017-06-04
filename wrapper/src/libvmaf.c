7faefc15d9939bef112b432b4242bc24f34693eb
ashk43712 <ashk43712@gmail.com>
2017-06-05 02:14:24 +0530

    CHanged makefile and libvmaf

---
+++ wrapper/src/libvmaf.c
@@ -0,0 +1,34 @@
+
+#include "libvmaf.h"
+
+double compute_vmaf(char* fmt, int width, int height, char *ref_path, char *dis_path)
+{
+	printf("hello");
+/*
+    char *model_path;
+    char *log_path = NULL;
+    char *log_fmt = NULL;
+    bool disable_clip = false;
+    bool disable_avx = false;
+    bool enable_transform = false;
+    bool do_psnr = false;
+    bool do_ssim = false;
+    bool do_ms_ssim = false;
+    char *pool_method = NULL;
+
+
+    try
+    {
+        double score = RunVmaf(fmt, width, height, ref_path, dis_path, model_path, log_path, log_fmt, disable_clip, enable_transform, do_psnr, do_ssim, do_ms_ssim, pool_method);
+
+        return score;
+    }
+    catch (const std::exception &e)
+    {
+        fprintf(stderr, "Error: %s\n", e.what());
+        return -1;
+    }
+*/
+return 0;
+
+}
--- wrapper/src/libvmaf.h
+++ wrapper/src/libvmaf.h
@@ -1,1 +1,6 @@
+#ifndef LIBVMAF_H_
+#define LIBVMAF_H_
+
 double compute_vmaf(char* fmt, int width, int height, char *ref_path, char *dis_path);
+
+#endif /* LIBVMAF_H_ */
