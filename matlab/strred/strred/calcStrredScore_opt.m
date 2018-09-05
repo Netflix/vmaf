4f1590cc053078b08454ebfb7002724a0fe74cb9
Christos Bampis <cbampis@gmail.com>
2018-09-05 13:04:57 -0700

    Add ST-RREDOpt (time optimized), ST-MAD feature extractors, quality runners and unittestts. Refactor ST-RRED feature extractor. (#216)

--- matlab/strred/strred/calcStrredScore_opt.m
+++ matlab/strred/strred/calcStrredScore_opt.m
@@ -1,34 +1,39 @@
-function calcStrredScore(rname,dname,rows,colms)
+function calcStrredScore_opt(rname, dname, rows, colms)
 
-rfid = fopen(rname);
-dfid = fopen(dname);
+band = 4;
+Nscales = 5;
+Nor = 6;
+blk = 3;
+sigma_nsq = 0.1;
 
 srred = [];
 trred = [];
 
+rfid = fopen(rname);
+dfid = fopen(dname);
+
 iframe = 0;
 while 1
     
-    [yr, cbr, crr] = readframefromfid(rfid, rows, colms);
-    [yd, cbr, crr] = readframefromfid(dfid, rows, colms);
+    [yr, ~, ~] = readframefromfid(rfid, rows, colms);
+    [yd, ~, ~] = readframefromfid(dfid, rows, colms);
     
     if feof(rfid) || feof(dfid)
         break;
     end
     
     % read successful, can then reshape
-    yr = reshape(yr, rows, colms)';
-    yd = reshape(yd, rows, colms)';
+    yr = reshape(yr, [colms rows])';
+    yd = reshape(yd, [colms rows])';
     
-    if iframe > 0
-        [spatial_ref temporal_ref] = extract_info(yr, yr_prev);
-        [spatial_dis temporal_dis] = extract_info(yd, yd_prev);
-        srred_now = mean2(abs(spatial_ref - spatial_dis));
-        trred_now = mean2(abs(temporal_ref - temporal_dis));
+    if iframe > 0        
+        [srred_now, ~, trred_now, ~] = extract_info_opt(yr, yr_prev, yd, ...
+            yd_prev, band, Nscales, Nor, ...
+            blk, sigma_nsq);
         srred = [srred srred_now];
         trred = [trred trred_now];
     end
-
+    
     yr_prev = yr;
     yd_prev = yd;
     iframe = iframe + 1;
@@ -42,7 +47,7 @@ srred = [srred(1) srred];
 trred = [trred(1) trred];
 
 for frame_ind = 0 : iframe - 1
-
+    
     disp(sprintf('srred: %d %f', frame_ind, srred(frame_ind + 1)));
     disp(sprintf('trred: %d %f', frame_ind, trred(frame_ind + 1)));
 
--- python/src/vmaf/core/matlab_quality_runner.py
+++ python/src/vmaf/core/matlab_quality_runner.py
@@ -1,5 +1,5 @@
 from vmaf.core.feature_assembler import FeatureAssembler
-from vmaf.core.matlab_feature_extractor import StrredFeatureExtractor, SpEEDMatlabFeatureExtractor
+from vmaf.core.matlab_feature_extractor import StrredFeatureExtractor, StrredOptFeatureExtractor, SpEEDMatlabFeatureExtractor, STMADFeatureExtractor
 from vmaf.core.quality_runner import QualityRunner
 from vmaf.core.result import Result
 
@@ -48,6 +48,56 @@ class StrredQualityRunner(QualityRunner):
         result_dict[self.get_scores_key()] = feature_result.result_dict[
             StrredFeatureExtractor.get_scores_key('strred')] # add strred score
         del result_dict[StrredFeatureExtractor.get_scores_key('strred')] # delete redundant
+        return Result(asset, self.executor_id, result_dict)
+
+    def _remove_result(self, asset):
+        # Override Executor._remove_result(self, asset) by redirecting it to the
+        # FeatureAssembler.
+
+        vmaf_fassembler = self._get_feature_assembler_instance(asset)
+        vmaf_fassembler.remove_results()
+
+
+class StrredOptQualityRunner(QualityRunner):
+
+    TYPE = 'STRREDOpt'
+
+    VERSION = 'F' + StrredOptFeatureExtractor.VERSION + '-1.1'
+
+    def _get_quality_scores(self, asset):
+        raise NotImplementedError
+
+    def _generate_result(self, asset):
+        raise NotImplementedError
+
+    def _get_feature_assembler_instance(self, asset):
+
+        feature_dict = {StrredOptFeatureExtractor.TYPE: StrredOptFeatureExtractor.ATOM_FEATURES + getattr(StrredOptFeatureExtractor, 'DERIVED_ATOM_FEATURES', [])}
+
+        feature_assembler = FeatureAssembler(
+            feature_dict=feature_dict,
+            feature_option_dict=None,
+            assets=[asset],
+            logger=self.logger,
+            fifo_mode=self.fifo_mode,
+            delete_workdir=self.delete_workdir,
+            result_store=self.result_store,
+            optional_dict=None,
+            optional_dict2=None,
+            parallelize=False, # parallelization already in a higher level
+        )
+        return feature_assembler
+
+    def _run_on_asset(self, asset):
+        # Override Executor._run_on_asset(self, asset)
+        vmaf_fassembler = self._get_feature_assembler_instance(asset)
+        vmaf_fassembler.run()
+        feature_result = vmaf_fassembler.results[0]
+        result_dict = {}
+        result_dict.update(feature_result.result_dict.copy()) # add feature result
+        result_dict[self.get_scores_key()] = feature_result.result_dict[
+            StrredOptFeatureExtractor.get_scores_key('strred')] # add strred score
+        del result_dict[StrredOptFeatureExtractor.get_scores_key('strred')] # delete redundant
         return Result(asset, self.executor_id, result_dict)
 
     def _remove_result(self, asset):
@@ -109,3 +159,52 @@ class SpEEDMatlabQualityRunner(QualityRunner):
 
             speed_fassembler = self._get_feature_assembler_instance(asset)
             speed_fassembler.remove_results()
+
+
+class STMADQualityRunner(QualityRunner):
+    TYPE = 'STMAD'
+
+    VERSION = 'F' + STMADFeatureExtractor.VERSION + '-1.1'
+
+    def _get_quality_scores(self, asset):
+        raise NotImplementedError
+
+    def _generate_result(self, asset):
+        raise NotImplementedError
+
+    def _get_feature_assembler_instance(self, asset):
+        feature_dict = {STMADFeatureExtractor.TYPE: STMADFeatureExtractor.ATOM_FEATURES + getattr(
+            STMADFeatureExtractor, 'DERIVED_ATOM_FEATURES', [])}
+
+        feature_assembler = FeatureAssembler(
+            feature_dict=feature_dict,
+            feature_option_dict=None,
+            assets=[asset],
+            logger=self.logger,
+            fifo_mode=self.fifo_mode,
+            delete_workdir=self.delete_workdir,
+            result_store=self.result_store,
+            optional_dict=None,
+            optional_dict2=None,
+            parallelize=False,  # parallelization already in a higher level
+        )
+        return feature_assembler
+
+    def _run_on_asset(self, asset):
+        # Override Executor._run_on_asset(self, asset)
+        vmaf_fassembler = self._get_feature_assembler_instance(asset)
+        vmaf_fassembler.run()
+        feature_result = vmaf_fassembler.results[0]
+        result_dict = {}
+        result_dict.update(feature_result.result_dict.copy())  # add feature result
+        result_dict[self.get_scores_key()] = feature_result.result_dict[
+            STMADFeatureExtractor.get_scores_key('stmad')]  # add strred score
+        del result_dict[STMADFeatureExtractor.get_scores_key('stmad')]  # delete redundant
+        return Result(asset, self.executor_id, result_dict)
+
+    def _remove_result(self, asset):
+        # Override Executor._remove_result(self, asset) by redirecting it to the
+        # FeatureAssembler.
+
+        vmaf_fassembler = self._get_feature_assembler_instance(asset)
+        vmaf_fassembler.remove_results()
