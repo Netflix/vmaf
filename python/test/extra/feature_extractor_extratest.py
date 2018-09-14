a654f6f7f5998aedf2d6e2f6b3a9eece9753c3fb
Zhi Li <zli@netflix.com>
2018-09-14 12:35:21 -0700

    Fix extratest.

--- python/test/extra/feature_extractor_extratest.py
+++ python/test/extra/feature_extractor_extratest.py
@@ -4,7 +4,7 @@ from vmaf.core.asset import Asset
 from vmaf.core.feature_extractor import VmafFeatureExtractor
 from vmaf.core.matlab_feature_extractor import StrredFeatureExtractor, StrredOptFeatureExtractor, SpEEDMatlabFeatureExtractor, STMADFeatureExtractor
 from vmaf.tools.stats import ListStats
-from vmaf.tools.testutil import set_default_576_324_videos_for_testing
+from testutil import set_default_576_324_videos_for_testing
 
 __copyright__ = "Copyright 2016-2018, Netflix, Inc."
 __license__ = "Apache, Version 2.0"
