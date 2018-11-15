f9ea448444b62319c1e204d3a1093b369319d7db
Zhi Li <zli@netflix.com>
2018-11-14 23:11:52 -0800

    Feature/add bd rate calculator (#261)
    * Add BD rate calculator.
    
    * Add docstring on source of validation for BD rate calculator.

---
+++ python/src/vmaf/tools/bd_rate_calculator.py
@@ -0,0 +1,133 @@
+import numpy as np
+
+from vmaf.tools.interpolation_utils import InterpolationUtils
+
+__copyright__ = "Copyright 2016-2018, Netflix, Inc."
+__license__ = "Apache, Version 2.0"
+
+
+class BDrateCalculator(object):
+    """
+    BD rate calculator. Implementation validated against JCTVC-E137:
+    http://phenix.it-sudparis.eu/jct/doc_end_user/documents/5_Geneva/wg11/JCTVC-E137-v1.zip
+    """
+
+    REJECTED_BD_RATE_NO_OVERLAP = -10000
+    REJECTED_BD_RATE_NON_MONOTONIC = -20000
+    REJECTED_BD_RATE_MESSED_UP_RATE = -30000
+    REJECTED_BALANCE_BAD_MEASUREMENTS = -40000
+
+    @staticmethod
+    def _dedup_and_order(set_):
+        return sorted(list(set(set_)), key=lambda x: x[0])
+
+    @classmethod
+    def CalcBDRate(cls, setA, setB):
+
+        # ==== added by zli =======
+        setA = cls._dedup_and_order(setA)
+        setB = cls._dedup_and_order(setB)
+        # ==== added by zli =======
+
+        assert not (len(setA) < 4 or len(setB) < 4), \
+            "Problem with input RD point lists. setA is size {}, " \
+            "setB is size {}".format(len(setA), len(setB))
+
+        if not cls.isCurveMonotonic(setA):
+            raise AssertionError(cls.REJECTED_BD_RATE_NON_MONOTONIC)
+
+        if not cls.isCurveMonotonic(setB):
+            raise AssertionError(cls.REJECTED_BD_RATE_NON_MONOTONIC)
+
+        if not cls.ratesLookOkay(setA):
+            raise AssertionError(cls.REJECTED_BD_RATE_MESSED_UP_RATE)
+
+        if not cls.ratesLookOkay(setB):
+            raise AssertionError(cls.REJECTED_BD_RATE_MESSED_UP_RATE)
+
+        minMainPSNR = setA[0][1]
+        maxMainPSNR = setA[-1][1]
+        minHighPSNR = setB[0][1]
+        maxHighPSNR = setB[-1][1]
+
+        minPSNR = max(minMainPSNR, minHighPSNR)
+        maxPSNR = min(maxMainPSNR, maxHighPSNR)
+
+        # no overlap, so mark it in a special way
+        if minPSNR >= maxPSNR:
+            return cls.REJECTED_BD_RATE_NO_OVERLAP
+
+        vA = cls.bdrint(setA, minPSNR, maxPSNR)
+        vB = cls.bdrint(setB, minPSNR, maxPSNR)
+
+        avg = (vB - vA) / (maxPSNR - minPSNR)
+        return np.power(10, avg) - 1
+
+    @staticmethod
+    def isCurveMonotonic(set_):
+        for i in range(len(set_) - 1):
+
+            # ==== added by zli =======
+            if set_[i][0] >= set_[i + 1][0]:
+                return False
+            # ==== added by zli =======
+
+            # ==== added by zli =======
+            # if set_[i][1] > set_[i + 1][1]:
+            if set_[i][1] >= set_[i + 1][1]:
+            # ==== added by zli =======
+                return False
+
+        return True
+
+    @staticmethod
+    def ratesLookOkay(set_):
+        for i in range(len(set_)):
+            if set_[i][0] == 0:
+                return False
+        return True
+
+    # // BD-rate calculation for arbitrary number (N) points
+    # // cf. https://www.mathworks.com/moler/interp.pdf, sections 3.3 - 3.4
+    @staticmethod
+    def bdrint(rdPointsList, minPSNR, maxPSNR):
+
+        N = len(rdPointsList)
+
+        log_rate = []
+        log_dist = []
+        H = []
+        delta = []
+        d = []
+        c = []
+        b = []
+
+        InterpolationUtils.computeParamsForSegments(rdPointsList, log_rate, log_dist, H, delta, d, c, b, True)
+
+        # // cubic function is rate(i) + s*(d(i) + s*(c(i) + s*(b(i))) where s = x - dist(i)
+        # // or rate(i) + s*d(i) + s*s*c(i) + s*s*s*b(i)
+        # // primitive is s*rate(i) + s*s*d(i)/2 + s*s*s*c(i)/3 + s*s*s*s*b(i)/4
+
+        result = 0.0
+
+        for i in range(N - 1):
+            s0 = log_dist[i]
+            s1 = log_dist[i + 1]
+
+            # // clip s0 to valid range
+            s0 = max(s0, minPSNR)
+            s0 = min(s0, maxPSNR)
+            s0 -= log_dist[i]
+
+            # // clip s1 to valid range
+            s1 = max(s1, minPSNR)
+            s1 = min(s1, maxPSNR)
+            s1 -= log_dist[i]
+
+            if s1 > s0:
+                result += (s1 - s0) * log_rate[i]
+                result += (s1 * s1 - s0 * s0) * d[i] / 2.0
+                result += (s1 * s1 * s1 - s0 * s0 * s0) * c[i] / 3.0
+                result += (s1 * s1 * s1 * s1 - s0 * s0 * s0 * s0) * b[i] / 4.0
+
+        return result
---
+++ python/src/vmaf/tools/interpolation_utils.py
@@ -0,0 +1,117 @@
+import numpy as np
+
+__copyright__ = "Copyright 2016-2018, Netflix, Inc."
+__license__ = "Apache, Version 2.0"
+
+
+class InterpolationUtils(object):
+
+    # /**
+    #  * Compute PCHIP params and compute the rate from distortion (or PSNR).
+    #  *
+    #  * @param rdPointsList        List of RD points, minimum 4 needed
+    #  * @param arrayDistInLogScale List of distortion values for which rate values are desired
+    #  * @return List of rate values, either in log scale or linear scale
+    #  */
+    @classmethod
+    def interpolateRateFromMetric(cls, rdPointsList, arrayDistInLogScale):
+
+        N = len(rdPointsList)
+
+        log_rate = []
+        log_dist = []
+        H = []
+        delta = []
+        d = []
+        c = []
+        b = []
+
+        cls.computeParamsForSegments(rdPointsList, log_rate, log_dist, H, delta, d, c, b, False)
+
+        arrayRate = []
+
+        for distInLogScale in arrayDistInLogScale:
+            distBegin = log_dist[0]
+            yk = log_rate[0]
+            segmentOfInterest = 0
+            for i in range(N-1):
+                if log_dist[i] <= distInLogScale and log_dist[i+1] >= distInLogScale:
+                    distBegin = log_dist[i]
+                    yk = log_rate[i]
+                    segmentOfInterest = i
+                    break
+
+            rate = cls.computeRate(distInLogScale, distBegin, yk, d[segmentOfInterest], c[segmentOfInterest], b[segmentOfInterest])
+            rate = min(rate, log_rate[segmentOfInterest + 1])
+            rate = max(rate, log_rate[segmentOfInterest]) # make sure interpolated rate is within bounds
+            arrayRate.append(rate)
+
+        return arrayRate
+
+    # /***
+    #  *
+    #  * Compute rate from distortion using formula. Use parameters for the curve segment that are passed in.
+    #  *
+    #  * @param x     distortion for which rate needs to be computed
+    #  * @param xk    distortion at the beginning of curve segment
+    #  * @param yk    rate (in log scale) at beginning of curve segment
+    #  * @param dk    d parameter of relevant curve segment
+    #  * @param ck    c parameter of relevant curve segment
+    #  * @param bk    b parameter of relevant curve segment
+    #  * @return rate in log scale computed using formula and passed in parameters
+    #  */
+    @staticmethod
+    def computeRate(x, xk, yk, dk, ck, bk):
+
+        s = x - xk
+
+        return yk + (s * dk) + (s * s * ck) + (s * s * s * bk)
+
+    @classmethod
+    def computeParamsForSegments(cls, rdPointsList, log_rate, log_dist, H, delta, d, c, b, convertRateUsingLogarithm):
+        N = len(rdPointsList)
+
+        for i in range(N):
+            if (convertRateUsingLogarithm):
+                log_rate.append(np.log10(rdPointsList[i][0]))
+            else:
+                log_rate.append(rdPointsList[i][0])
+            log_dist.append(rdPointsList[i][1])
+
+        # // H_i = length of i_th subinterval
+        # // delta_i = slope of i_th subinterval
+        for i in range(N - 1):
+            H.append(log_dist[i + 1] - log_dist[i])
+            delta.append((log_rate[i + 1] - log_rate[i]) / H[i])
+
+        # // Determine slopes at each point = coefficient of first-order term
+
+        # // determine slope at starting point
+        d.append(cls.pchipend(H[0], H[1], delta[0], delta[1]))
+
+        # // determine slope at all intermediate points: slope is weighted harmonic mean of two slopes (delta_{i-1} and delta_i)
+        for i in range(1, N-1):
+            d.append((3 * H[i - 1] + 3 * H[i]) / ((2 * H[i] + H[i - 1]) / delta[i - 1] + (H[i] + 2 * H[i - 1]) / delta[i]))
+
+        # // determine slope at end point
+        d.append(cls.pchipend(H[N - 2], H[N - 3], delta[N - 2], delta[N - 3]))
+
+        # // determine second- and third-order coefficients for each segment
+
+        for i in range(N - 1):
+            c.append((3 * delta[i] - 2 * d[i] - d[i + 1]) / H[i])
+            b.append((d[i] - 2 * delta[i] + d[i + 1]) / (H[i] * H[i]))
+
+    @staticmethod
+    def pchipend(h1, h2, delta1, delta2):
+        # // one-sided formula for endpoints
+        # // non-centered shape-preserving three point formula
+        d = ((2 * h1 + h2) * delta1 - h1 * delta2) / (h1 + h2)
+
+        # // opposite slopes -> set slope to zero
+        if d * delta1 < 0:
+            d = 0
+        elif (delta1 * delta2 < 0) and (abs(d) > abs(3 * delta1)):
+            d = 3 * delta1
+
+        return d
