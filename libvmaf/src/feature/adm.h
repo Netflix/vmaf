00944185910dfbc27cebb7fae6dd5b2442f315e4
Kyle Swanson <kswanson@netflix.com>
2020-01-17 10:37:42 -0800

    libvmaf: implement vmaf_fex_float_adm

---
+++ libvmaf/src/feature/adm.h
@@ -0,0 +1,4 @@
+int compute_adm(const float *ref, const float *dis, int w, int h,
+                int ref_stride, int dis_stride, double *score,
+                double *score_num, double *score_den, double *scores,
+                double border_factor);
