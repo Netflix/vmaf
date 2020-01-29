fc7d9a24bfece0a86b70fb6bcd18e07d6b9bfbb3
Kyle Swanson <kswanson@netflix.com>
2020-01-29 12:31:23 -0800

    libvmaf: add pixel offset for float feature extractors

--- libvmaf/src/feature/picture_copy.c
+++ libvmaf/src/feature/picture_copy.c
@@ -2,14 +2,14 @@
 
 #include <libvmaf/picture.h>
 
-void picture_copy(float *dst, VmafPicture *src)
+void picture_copy(float *dst, VmafPicture *src, int offset)
 {
     float *float_data = dst;
     uint8_t *data = src->data[0];
 
     for (unsigned i = 0; i < src->h[0]; i++) {
         for (unsigned j = 0; j < src->w[0]; j++) {
-            float_data[j] = (float) data[j];
+            float_data[j] = (float) data[j] + offset;
         }
         float_data += src->w[0];
         data += src->stride[0];
