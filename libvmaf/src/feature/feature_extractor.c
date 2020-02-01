923ce9bd61effe95a2099b1a6940ce8874c49a49
Kyle Swanson <kswanson@netflix.com>
2020-01-31 16:36:47 -0800

    libvmaf: implement vmaf_fex_float_ms_ssim

--- libvmaf/src/feature/feature_extractor.c
+++ libvmaf/src/feature/feature_extractor.c
@@ -11,6 +11,7 @@ extern VmafFeatureExtractor vmaf_fex_float_psnr;
 extern VmafFeatureExtractor vmaf_fex_float_adm;
 extern VmafFeatureExtractor vmaf_fex_float_vif;
 extern VmafFeatureExtractor vmaf_fex_float_motion;
+extern VmafFeatureExtractor vmaf_fex_float_ms_ssim;
 
 static VmafFeatureExtractor *feature_extractor_list[] = {
     &vmaf_fex_ssim,
@@ -20,6 +21,7 @@ static VmafFeatureExtractor *feature_extractor_list[] = {
     &vmaf_fex_float_adm,
     &vmaf_fex_float_vif,
     &vmaf_fex_float_motion,
+    &vmaf_fex_float_ms_ssim,
     NULL
 };
 
