d7f0f56b1a4e8708f2e5971e59292d1175177d4e
Christos Bampis <christosb@netflix.com>
2020-02-05 18:25:42 -0800

    Fix default behavior to enable clipping.

--- libvmaf/include/libvmaf/model.h
+++ libvmaf/include/libvmaf/model.h
@@ -4,13 +4,11 @@
 typedef struct VmafModel VmafModel;
 
 enum VmafModelFlags {
-    VMAF_MODEL_FLAG_UNKNOWN = 0,
+    VMAF_MODEL_FLAGS_DEFAULT = 0,
     VMAF_MODEL_FLAG_DISABLE_CLIP = (1 << 0),
     VMAF_MODEL_FLAG_ENABLE_TRANSFORM = (1 << 1),
     VMAF_MODEL_FLAG_ENABLE_CONFIDENCE_INTERVAL = (1 << 2),
 };
-
-#define VMAF_MODEL_FLAGS_DEFAULT VMAF_MODEL_FLAG_DISABLE_CLIP
 
 typedef struct VmafModelConfig {
     enum VmafModelFlags flags;
--- libvmaf/src/unpickle.cpp
+++ libvmaf/src/unpickle.cpp
@@ -23,13 +23,6 @@ static int unpickle(VmafModel *model, const char *pickle_path,
     Val score_clip = pickle_model["model_dict"]["score_clip"];
     Val score_transform = pickle_model["model_dict"]["score_transform"];
 
-    /* if flags are not set, error out */
-    if (flags == VMAF_MODEL_FLAG_UNKNOWN) {
-        fprintf(stderr, "Model flags need to be set in the model config. "
-                "For default behavior, use VMAF_MODEL_FLAGS_DEFAULT.\n");
-        return -EINVAL;
-    }
-
     if (!((VAL_IS_NONE(score_clip)) || VAL_IS_LIST(score_clip)))
         return -EINVAL;
     model->score_clip.enabled = !(VAL_IS_NONE(score_clip)) &&
