b5348a6e09aab39378ee8c80056ccb81cb697d42
Zhi Li <zli@netflix.com>
2020-05-29 09:01:12 -0700

    Change adm_dwt2_cy back to double precision.

--- python/test/cy_test.py
+++ python/test/cy_test.py
@@ -15,20 +15,20 @@ class AdmDwt2CyTest(unittest.TestCase):
 
     def test_adm_dwt2_cy(self):
         np.random.seed(0)
-        x = np.random.uniform(low=-128, high=127, size=[324, 576]).astype(np.float32)
+        x = np.random.uniform(low=-128, high=127, size=[324, 576]).astype(np.float64)
         a, v, h, d = adm_dwt2_cy(x)
         self.assertEqual(a.shape, (162, 288))
         self.assertEqual(v.shape, (162, 288))
         self.assertEqual(h.shape, (162, 288))
         self.assertEqual(d.shape, (162, 288))
-        self.assertAlmostEqual(np.float(np.std(a)), 73.94279479980469, places=5)
+        self.assertAlmostEqual(np.float(np.std(a)), 73.94278958581368, places=5)
         self.assertAlmostEqual(np.float(np.std(v)), 73.61917114257812, places=5)
-        self.assertAlmostEqual(np.float(np.std(h)), 73.30350494384766, places=5)
+        self.assertAlmostEqual(np.float(np.std(h)), 73.30349892103438, places=5)
         self.assertAlmostEqual(np.float(np.std(d)), 73.19024658203125, places=5)
 
     def test_adm_dwt2_py(self):
         np.random.seed(0)
-        x = np.random.uniform(low=-128, high=127, size=[324, 576]).astype(np.float32)
+        x = np.random.uniform(low=-128, high=127, size=[324, 576]).astype(np.float64)
         a, ds = adm_dwt2_py(x)
         h, v, d = ds
         self.assertEqual(a.shape, (162, 288))
@@ -40,10 +40,9 @@ class AdmDwt2CyTest(unittest.TestCase):
         self.assertAlmostEqual(np.float(np.std(h)), 73.53559112548828, places=5)
         self.assertAlmostEqual(np.float(np.std(d)), 73.52173614501953, places=5)
 
-    @unittest.skip
     def test_adm_dwt2_cy_small(self):
         np.random.seed(0)
-        x = np.random.uniform(low=-128, high=127, size=[36, 44]).astype(np.float32)
+        x = np.random.uniform(low=-128, high=127, size=[36, 44]).astype(np.float64)
         a, v, h, d = adm_dwt2_cy(x)
         self.assertEqual(a.shape, (18, 22))
         self.assertEqual(v.shape, (18, 22))
@@ -54,10 +53,9 @@ class AdmDwt2CyTest(unittest.TestCase):
         self.assertAlmostEqual(np.float(np.std(h)), 71.96445272697756, places=5)
         self.assertAlmostEqual(np.float(np.std(d)), 67.12256460863848, places=5)
 
-    @unittest.skip
     def test_adm_dwt2_cy_xsmall(self):
         np.random.seed(0)
-        x = np.random.uniform(low=-128, high=127, size=[18, 22]).astype(np.float32)
+        x = np.random.uniform(low=-128, high=127, size=[18, 22]).astype(np.float64)
         a, v, h, d = adm_dwt2_cy(x)
         self.assertEqual(a.shape, (9, 11))
         self.assertEqual(v.shape, (9, 11))
@@ -76,12 +74,12 @@ class AdmDwt2CyTestOnAkiyo(unittest.TestCase):
                 filepath=VmafConfig.test_resource_path("yuv", "refp_vmaf_hacking_investigation_0_0_akiyo_cif_notyuv_0to0_identity_vs_akiyo_cif_notyuv_0to0_multiply_q_352x288"),
                 width=352, height=288, yuv_type='yuv420p'
         ) as yuv_reader_ref:
-            self.y_ref = yuv_reader_ref.next()[0].astype(np.float32)
+            self.y_ref = yuv_reader_ref.next()[0].astype(np.float64)
         with YuvReader(
                 filepath=VmafConfig.test_resource_path("yuv", "disp_vmaf_hacking_investigation_0_0_akiyo_cif_notyuv_0to0_identity_vs_akiyo_cif_notyuv_0to0_multiply_q_352x288"),
                 width=352, height=288, yuv_type='yuv420p'
         ) as yuv_reader_ref:
-            self.y_dis = yuv_reader_ref.next()[0].astype(np.float32)
+            self.y_dis = yuv_reader_ref.next()[0].astype(np.float64)
 
     def test_adm_dwt2_cy_on_akiyo_single_scale(self):
 
@@ -150,30 +148,30 @@ class AdmDwt2CyTestOnAkiyo(unittest.TestCase):
         self.assertEqual(v.shape, (36, 44))
         self.assertEqual(h.shape, (36, 44))
         self.assertEqual(d.shape, (36, 44))
-        self.assertAlmostEqual(np.float(np.std(a)), 388.2596130371094, places=5)
-        self.assertAlmostEqual(np.float(np.std(v)), 49.79795455932617, places=5)
-        self.assertAlmostEqual(np.float(np.std(h)), 44.297210693359375, places=5)
-        self.assertAlmostEqual(np.float(np.std(d)), 21.884857177734375, places=5)
+        self.assertAlmostEqual(np.float(np.std(a)), 417.73602985019386, places=5)
+        self.assertAlmostEqual(np.float(np.std(v)), 56.12149501761274, places=5)
+        self.assertAlmostEqual(np.float(np.std(h)), 45.419327754665, places=5)
+        self.assertAlmostEqual(np.float(np.std(d)), 23.307968058054456, places=5)
 
         a, v, h, d = adm_dwt2_cy(a)
         self.assertEqual(a.shape, (18, 22))
         self.assertEqual(v.shape, (18, 22))
         self.assertEqual(h.shape, (18, 22))
         self.assertEqual(d.shape, (18, 22))
-        self.assertAlmostEqual(np.float(np.std(a)), 615.3699951171875, places=5)
-        self.assertAlmostEqual(np.float(np.std(v)), 93.22431945800781, places=5)
-        self.assertAlmostEqual(np.float(np.std(h)), 336.55194091796875, places=5)
-        self.assertAlmostEqual(np.float(np.std(d)), 111.64820861816406, places=5)
+        self.assertAlmostEqual(np.float(np.std(a)), 743.8105722045989, places=5)
+        self.assertAlmostEqual(np.float(np.std(v)), 116.11048356419965, places=5)
+        self.assertAlmostEqual(np.float(np.std(h)), 112.95438137920729, places=5)
+        self.assertAlmostEqual(np.float(np.std(d)), 45.10849973956695, places=5)
 
         a, v, h, d = adm_dwt2_cy(a)
         self.assertEqual(a.shape, (9, 11))
         self.assertEqual(v.shape, (9, 11))
         self.assertEqual(h.shape, (9, 11))
         self.assertEqual(d.shape, (9, 11))
-        self.assertAlmostEqual(np.float(np.std(a)), 814.2235717773438, places=5)
-        self.assertAlmostEqual(np.float(np.std(v)), 152.11126708984375, places=5)
-        self.assertAlmostEqual(np.float(np.std(h)), 451.4018249511719, places=5)
-        self.assertAlmostEqual(np.float(np.std(d)), 209.05331420898438, places=5)
+        self.assertAlmostEqual(np.float(np.std(a)), 1086.6252584211409, places=5)
+        self.assertAlmostEqual(np.float(np.std(v)), 231.09377461005127, places=5)
+        self.assertAlmostEqual(np.float(np.std(h)), 562.4967265458288, places=5)
+        self.assertAlmostEqual(np.float(np.std(d)), 324.7682467339999, places=5)
 
 
 class AdmDwt2CyTestOnAkiyoXsmall(unittest.TestCase):
@@ -183,35 +181,35 @@ class AdmDwt2CyTestOnAkiyoXsmall(unittest.TestCase):
                 filepath=VmafConfig.test_resource_path("yuv", "refp_vmaf_hacking_investigation_0_0_akiyo_cif_notyuv_0to0_identity_vs_akiyo_cif_notyuv_0to0_multiply_q_18x22"),
                 width=22, height=18, yuv_type='yuv420p'
         ) as yuv_reader_ref:
-            self.y_ref = yuv_reader_ref.next()[0].astype(np.float32)
+            self.y_ref = yuv_reader_ref.next()[0].astype(np.float64)
         with YuvReader(
                 filepath=VmafConfig.test_resource_path("yuv", "disp_vmaf_hacking_investigation_0_0_akiyo_cif_notyuv_0to0_identity_vs_akiyo_cif_notyuv_0to0_multiply_q_18x22"),
                 width=22, height=18, yuv_type='yuv420p'
         ) as yuv_reader_ref:
-            self.y_dis = yuv_reader_ref.next()[0].astype(np.float32)
+            self.y_dis = yuv_reader_ref.next()[0].astype(np.float64)
 
     def test_adm_dwt2_cy_on_akiyo_single_scale(self):
 
-        a, v, h, d = adm_dwt2_cy((self.y_ref - 128.0).astype(np.float32))
+        a, v, h, d = adm_dwt2_cy((self.y_ref - 128.0).astype(np.float64))
         self.assertEqual(a.shape, (9, 11))
         self.assertEqual(v.shape, (9, 11))
         self.assertEqual(h.shape, (9, 11))
         self.assertEqual(d.shape, (9, 11))
-        self.assertAlmostEqual(np.float(np.std(a)), 59.79219055175781, places=5)
-        self.assertAlmostEqual(np.float(np.std(v)), 12.54729175567627, places=5)
-        self.assertAlmostEqual(np.float(np.std(h)), 35.61844253540039, places=5)
-        self.assertAlmostEqual(np.float(np.std(d)), 15.827298164367676, places=5)
-        self.assertAlmostEqual(np.float(np.mean(a)), -75.7516098022461, places=5)
-        self.assertAlmostEqual(np.float(np.mean(v)), -2.64847469329834, places=5)
-        self.assertAlmostEqual(np.float(np.mean(h)), -0.5888747572898865, places=5)
-        self.assertAlmostEqual(np.float(np.mean(d)), -0.11442910879850388, places=5)
+        self.assertAlmostEqual(np.float(np.std(a)), 68.79884774616018, places=5)
+        self.assertAlmostEqual(np.float(np.std(v)), 13.512513212683404, places=5)
+        self.assertAlmostEqual(np.float(np.std(h)), 44.15490291235366, places=5)
+        self.assertAlmostEqual(np.float(np.std(d)), 17.07667660487361, places=5)
+        self.assertAlmostEqual(np.float(np.mean(a)), -75.92569837539651, places=5)
+        self.assertAlmostEqual(np.float(np.mean(v)), -3.5294110328601627, places=5)
+        self.assertAlmostEqual(np.float(np.mean(h)), 1.1199154418499353, places=5)
+        self.assertAlmostEqual(np.float(np.mean(d)), -0.48905969923597864, places=5)
 
-        self.assertAlmostEqual(a[0][0], -145.910904, places=6)
-        self.assertAlmostEqual(a[0][-1], -140.545441, places=6)
+        self.assertAlmostEqual(a[0][0], -145.9108983848698, places=6)
+        self.assertAlmostEqual(a[0][-1], -140.54543100196207, places=6)
         self.assertAlmostEqual(a[1][0], 0.0, places=6)
-        self.assertAlmostEqual(a[1][-1], -113.189041, places=6)
-        self.assertAlmostEqual(a[-1][0], -138.68803, places=5)
-        self.assertAlmostEqual(a[-1][-1], -166.480515, places=6)
+        self.assertAlmostEqual(a[1][-1], -103.95207793609761, places=6)
+        self.assertAlmostEqual(a[-1][0], 106.86207299548309, places=5)
+        self.assertAlmostEqual(a[-1][-1], 50.15606578219574, places=6)
 
 
 if __name__ == '__main__':
--- python/vmaf/core/adm_dwt2_cy.pyx
+++ python/vmaf/core/adm_dwt2_cy.pyx
@@ -1,4 +1,4 @@
-import sys
+# import sys
 
 import numpy as np
 cimport numpy as np
@@ -7,7 +7,7 @@ from libc.stdlib cimport calloc, free
 
 from vmaf.core.adm_dwt2_tools import ALIGN_CEIL, MAX_ALIGN
 
-ctypedef float np_float
+ctypedef double np_float
 
 cdef struct adm_dwt_band_t:
     np_float *band_a
@@ -17,10 +17,10 @@ cdef struct adm_dwt_band_t:
 
 cdef extern from "../../../libvmaf/src/feature/adm_tools.c":
     void dwt2_src_indices_filt_s(int **src_ind_y, int **src_ind_x, int w, int h)
-    void adm_dwt2_s(const float *src, const adm_dwt_band_t *dst, int **ind_y, int **ind_x, int w, int h, int src_stride, int dst_stride)
+    void adm_dwt2_d(const np_float *src, const adm_dwt_band_t *dst, int **ind_y, int **ind_x, int w, int h, int src_stride, int dst_stride)
 
 cdef extern from "../../../libvmaf/src/feature/adm.c":
-    char *init_dwt_band(adm_dwt_band_t *band, char *data_top, size_t buf_sz_one)
+    char *init_dwt_band_d(adm_dwt_band_t *band, char *data_top, size_t buf_sz_one)
 
 cdef extern from "../../../libvmaf/src/mem.c":
     void *aligned_malloc(size_t size, size_t alignment)
@@ -29,9 +29,9 @@ cdef extern from "../../../libvmaf/src/mem.c":
 cdef extern from "../../../libvmaf/src/feature/offset.c":
     int offset_image_s(float *buf, float off, int width, int height, int stride)  # TODO: find out why need this _s - doesn't seem to be called but if not, symbol not found error
 
-def adm_dwt2_cy(np.ndarray[np.float32_t, ndim=2, mode='c'] a):
+def adm_dwt2_cy(np.ndarray[np.float64_t, ndim=2, mode='c'] a):
 
-    cdef np.ndarray[np.float32_t, ndim=2, mode='c'] a_buf = np.ascontiguousarray(a, dtype=np.float32)
+    cdef np.ndarray[np.float64_t, ndim=2, mode='c'] a_buf = np.ascontiguousarray(a, dtype=np.float64)
     cdef np_float *aa = <np_float*> a_buf.data  # aa: curr_ref_scale
 
     cdef int h = len(a)
@@ -57,22 +57,22 @@ def adm_dwt2_cy(np.ndarray[np.float32_t, ndim=2, mode='c'] a):
     cdef int ind_size_y = ALIGN_CEIL(((h + 1) // 2) * sizeof(int))
     cdef int ind_size_x = ALIGN_CEIL(((w + 1) // 2) * sizeof(int))
 
-    # == # must use calloc to initialize mem to 0: adm_dwt2_s doesn't touch every cell for small sizes ==
+    # == # must use calloc to initialize mem to 0: adm_dwt2_s doesn't touch every cell for small w and h ==
     # data_buf = <np_float *> aligned_malloc(buf_sz_one * 4, MAX_ALIGN)
     data_buf = <np_float *> calloc(buf_sz_one * 4, 1)
     if not data_buf:
         free(data_buf)
-        free(buf_y_orig)
-        free(buf_x_orig)
+        aligned_free(buf_y_orig)
+        aligned_free(buf_x_orig)
         raise MemoryError
     data_top = <char *>data_buf
-    data_top = init_dwt_band(&aa_dwt2, data_top, buf_sz_one)
+    data_top = init_dwt_band_d(&aa_dwt2, data_top, buf_sz_one)
 
     buf_y_orig = <char *> aligned_malloc(ind_size_y * 4, MAX_ALIGN)
     if not buf_y_orig:
         free(data_buf)
-        free(buf_y_orig)
-        free(buf_x_orig)
+        aligned_free(buf_y_orig)
+        aligned_free(buf_x_orig)
         raise MemoryError
     ind_buf_y = <char *>buf_y_orig
     ind_y[0] = <int *> ind_buf_y; ind_buf_y += ind_size_y
@@ -83,8 +83,8 @@ def adm_dwt2_cy(np.ndarray[np.float32_t, ndim=2, mode='c'] a):
     buf_x_orig = <char *> aligned_malloc(ind_size_x * 4, MAX_ALIGN)
     if not buf_x_orig:
         free(data_buf)
-        free(buf_y_orig)
-        free(buf_x_orig)
+        aligned_free(buf_y_orig)
+        aligned_free(buf_x_orig)
         raise MemoryError
     ind_buf_x = <char *>buf_x_orig
     ind_x[0] = <int *> ind_buf_x; ind_buf_x += ind_size_x
@@ -92,11 +92,11 @@ def adm_dwt2_cy(np.ndarray[np.float32_t, ndim=2, mode='c'] a):
     ind_x[2] = <int *> ind_buf_x; ind_buf_x += ind_size_x
     ind_x[3] = <int *> ind_buf_x; ind_buf_x += ind_size_x
 
-    cdef np.ndarray[np.float32_t, ndim=2, mode='c'] out_a, out_v, out_h, out_d
+    cdef np.ndarray[np.float64_t, ndim=2, mode='c'] out_a, out_v, out_h, out_d
 
     try:
         dwt2_src_indices_filt_s(ind_y, ind_x, w, h)
-        adm_dwt2_s(aa, &aa_dwt2, ind_y, ind_x, w, h, curr_ref_stride, buf_stride)
+        adm_dwt2_d(aa, &aa_dwt2, ind_y, ind_x, w, h, curr_ref_stride, buf_stride)
 
         w_new = (w + 1) // 2
         h_new = (h + 1) // 2
@@ -107,29 +107,29 @@ def adm_dwt2_cy(np.ndarray[np.float32_t, ndim=2, mode='c'] a):
         # print("curr_ref_stride={}, buf_stride={}, buf_sz_one={}".format(curr_ref_stride, buf_stride, buf_sz_one))
         # print("ind_size_y={}, ind_size_x={}".format(ind_size_y, ind_size_x))
         # print("h_new={}, w_new={}".format(h_new, w_new))
-        # print("np.std(aa_band.band_a)={}".format(np.std(np.asarray(<np.float32_t[:h_new, :w_new]> aa_dwt2.band_a))))
-        # print("np.std(aa_band.band_v)={}".format(np.std(np.asarray(<np.float32_t[:h_new, :w_new]> aa_dwt2.band_v))))
-        # print("np.std(aa_band.band_h)={}".format(np.std(np.asarray(<np.float32_t[:h_new, :w_new]> aa_dwt2.band_h))))
-        # print("np.std(aa_band.band_d)={}".format(np.std(np.asarray(<np.float32_t[:h_new, :w_new]> aa_dwt2.band_d))))
+        # print("np.std(aa_band.band_a)={}".format(np.std(np.asarray(<np.float64_t[:h_new, :w_new]> aa_dwt2.band_a))))
+        # print("np.std(aa_band.band_v)={}".format(np.std(np.asarray(<np.float64_t[:h_new, :w_new]> aa_dwt2.band_v))))
+        # print("np.std(aa_band.band_h)={}".format(np.std(np.asarray(<np.float64_t[:h_new, :w_new]> aa_dwt2.band_h))))
+        # print("np.std(aa_band.band_d)={}".format(np.std(np.asarray(<np.float64_t[:h_new, :w_new]> aa_dwt2.band_d))))
         # for i in range(99):  # for 11 x 9
         #     sys.stdout.write("{}\t".format(aa_dwt2.band_a[i]))
         #     if i%10 == 0:
         #         sys.stdout.write("\n")
         # sys.stdout.write("\n")
 
-        out_a = np.empty((h_new, w_new)).astype(np.float32)
-        out_v = np.empty((h_new, w_new)).astype(np.float32)
-        out_h = np.empty((h_new, w_new)).astype(np.float32)
-        out_d = np.empty((h_new, w_new)).astype(np.float32)
+        out_a = np.empty((h_new, w_new)).astype(np.float64)
+        out_v = np.empty((h_new, w_new)).astype(np.float64)
+        out_h = np.empty((h_new, w_new)).astype(np.float64)
+        out_d = np.empty((h_new, w_new)).astype(np.float64)
 
-        out_a[...] = np.asarray(<np.float32_t[:h_new, :w_new]> aa_dwt2.band_a)[...]
-        out_v[...] = np.asarray(<np.float32_t[:h_new, :w_new]> aa_dwt2.band_v)[...]
-        out_h[...] = np.asarray(<np.float32_t[:h_new, :w_new]> aa_dwt2.band_h)[...]
-        out_d[...] = np.asarray(<np.float32_t[:h_new, :w_new]> aa_dwt2.band_d)[...]
+        out_a[...] = np.asarray(<np.float64_t[:h_new, :w_new]> aa_dwt2.band_a)[...]
+        out_v[...] = np.asarray(<np.float64_t[:h_new, :w_new]> aa_dwt2.band_v)[...]
+        out_h[...] = np.asarray(<np.float64_t[:h_new, :w_new]> aa_dwt2.band_h)[...]
+        out_d[...] = np.asarray(<np.float64_t[:h_new, :w_new]> aa_dwt2.band_d)[...]
 
     finally:
         free(data_buf)
-        free(buf_y_orig)
-        free(buf_x_orig)
+        aligned_free(buf_y_orig)
+        aligned_free(buf_x_orig)
 
     return out_a, out_v, out_h, out_d
