Fixed THR calculation

python/test/perf_metric_test.py
@@ -101,7 +101,7 @@ class AggrScorePerfMetricTest(unittest.TestCase):
         self.assertAlmostEqual(result['AUC_BW'], 0.9999999999999999, places=6)
         self.assertAlmostEqual(result['AUC_DS'], 0.9375, places=6)
         self.assertAlmostEqual(result['CC_0'], 1.0, places=6)
-        self.assertAlmostEqual(result['THR'], 3.0, places=6)
+        self.assertAlmostEqual(result['THR'], 1.0, places=6)
 
     def test_auc_metrics_performance(self):
         mat_filepath = VmafConfig.test_resource_path('data_Toyama.mat')
@@ -110,7 +110,7 @@ class AggrScorePerfMetricTest(unittest.TestCase):
         self.assertAlmostEqual(np.float(np.mean(results['AUC_DS'])), 0.69767003960902052, places=6)
         self.assertAlmostEqual(np.float(np.mean(results['AUC_BW'])), 0.94454700301894534, places=6)
         self.assertAlmostEqual(np.float(np.mean(results['CC_0'])), 0.88105386206276415, places=6)
-        self.assertAlmostEqual(np.float(np.mean(results['THR'])), 6.2392849606450556, places=6)
+        self.assertAlmostEqual(np.float(np.mean(results['THR'])), 3.899105581509778, places=6)
 
     def test_respow_perf_metric(self):
         np.random.seed(0)

python/vmaf/core/perf_metric.py
@@ -182,7 +182,7 @@ class AucPerfMetric(RawScorePerfMetric):
         pDS_HM, CI_DS = significanceHM(S, D, AUC_DS)
 
         # THR = prctile(D',95);
-        THR = np.percentile(D, 95, axis=1)
+        THR = np.percentile(S, 95, axis=1)
 
         # %%%%%%%%%%%%%%%%%%%%%%% Better / Worse %%%%%%%%%%%%%%%%%%%%%%%%%%%
 