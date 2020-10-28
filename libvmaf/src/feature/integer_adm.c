192d059f1bbeeb5420deb1569d2f028aaf10e559
Kyle Swanson <kswanson@netflix.com>
2020-10-27 18:42:08 -0700

    feature/x86: provide avx2 optimizations for adm
    OLD:
    VMAF version 7e4c8727
    500 frames ⠀⢐ 27.10 FPS
    
    NEW:
    VMAF version 7e4c8727
    500 frames ⠀⢐ 38.69 FPS

--- libvmaf/src/feature/integer_adm.c
+++ libvmaf/src/feature/integer_adm.c
@@ -16,160 +16,24 @@
  *
  */
 
-#include <errno.h>
-#include <math.h>
-#include <string.h>
 
 #include "feature_collector.h"
 #include "feature_extractor.h"
 
-#include "mem.h"
-#include "adm_options.h"
+#include "cpu.h"
+#include "integer_adm.h"
 
-static int32_t div_lookup[65536];
-static const int32_t div_Q_factor = 1073741824;   //2^30
-
-static inline void div_lookup_generator()
-{
-    for (int i = 1; i <= 32768; ++i)
-    {
-        int32_t recip = (int32_t)(div_Q_factor / i);
-        div_lookup[32768 + i] = recip;
-        div_lookup[32768 - i] = 0 - recip;
-    }
-}
-
-typedef struct adm_dwt_band_t {
-    int16_t *band_a; /* Low-pass V + low-pass H. */
-    int16_t *band_v; /* Low-pass V + high-pass H. */
-    int16_t *band_h; /* High-pass V + low-pass H. */
-    int16_t *band_d; /* High-pass V + high-pass H. */
-} adm_dwt_band_t;
-
-typedef struct i4_adm_dwt_band_t {
-    int32_t *band_a; /* Low-pass V + low-pass H. */
-    int32_t *band_v; /* Low-pass V + high-pass H. */
-    int32_t *band_h; /* High-pass V + low-pass H. */
-    int32_t *band_d; /* High-pass V + high-pass H. */
-} i4_adm_dwt_band_t;
-
-typedef struct AdmBuffer {
-    size_t ind_size_x, ind_size_y;  //strides size for intermidate buffers
-    void *data_buf;                //buffer for adm intermidiate data calculations
-    void *tmp_ref;                 //buffer for adm intermidiate data calculations
-    void *buf_x_orig;              //buffer for storing imgcoeff values along x.
-    void *buf_y_orig;              //buffer for storing imgcoeff values along y.
-    int *ind_y[4], *ind_x[4];
-
-    adm_dwt_band_t ref_dwt2;
-    adm_dwt_band_t dis_dwt2;
-    adm_dwt_band_t decouple_r;
-    adm_dwt_band_t decouple_a;
-    adm_dwt_band_t csf_a;
-    adm_dwt_band_t csf_f;
-
-    i4_adm_dwt_band_t i4_ref_dwt2;
-    i4_adm_dwt_band_t i4_dis_dwt2;
-    i4_adm_dwt_band_t i4_decouple_r;
-    i4_adm_dwt_band_t i4_decouple_a;
-    i4_adm_dwt_band_t i4_csf_a;
-    i4_adm_dwt_band_t i4_csf_f;
-} AdmBuffer;
+#if ARCH_X86
+#include "x86/adm_avx2.h"
+#endif
 
 typedef struct AdmState {
     size_t integer_stride;
     AdmBuffer buf;
+    void (*dwt2_8)(const uint8_t *src, const adm_dwt_band_t *dst,
+                   AdmBuffer *buf, int w, int h, int src_stride,
+                   int dst_stride);
 } AdmState;
-
-#ifndef NUM_BUFS_ADM
-#define NUM_BUFS_ADM 30
-#endif
-
-#ifndef M_PI
-#define M_PI 3.14159265358979323846264338327
-#endif // M_PI
-
-#ifndef ADM_BORDER_FACTOR
-#define ADM_BORDER_FACTOR (0.1)
-#endif // !ADM_BORDER_FACTOR
-
-
-#define DIVS(n, d) ((n) / (d))
-
-static const int16_t dwt2_db2_coeffs_lo[4] = { 15826, 27411, 7345, -4240 };
-static const int16_t dwt2_db2_coeffs_hi[4] = { -4240, -7345, 27411, -15826 };
-
-static const int32_t dwt2_db2_coeffs_lo_sum = 46342;
-static const int32_t dwt2_db2_coeffs_hi_sum = 0;
-
-#ifndef ONE_BY_15
-#define ONE_BY_15 8738
-#endif
-
-#ifndef I4_ONE_BY_15
-#define I4_ONE_BY_15 286331153
-#endif
-
-/* ================= */
-/* Noise floor model */
-/* ================= */
-
-#define VIEW_DIST 3.0f
-
-#define REF_DISPLAY_HEIGHT 1080
-
-/*
- * The following dwt visibility threshold parameters are taken from
- * "Visibility of Wavelet Quantization Noise"
- * by A. B. Watson, G. Y. Yang, J. A. Solomon and J. Villasenor
- * IEEE Trans. on Image Processing, Vol. 6, No 8, Aug. 1997
- * Page 1170, formula (7) and corresponding Table IV
- * Table IV has 2 entries for Cb and Cr thresholds
- * Chose those corresponding to subject "sfl" since they are lower
- * These thresholds were obtained and modeled for the 7-9 biorthogonal wavelet basis
- */
-
- /*
-  * The following dwt visibility threshold parameters are taken from
-  * "Visibility of Wavelet Quantization Noise"
-  * by A. B. Watson, G. Y. Yang, J. A. Solomon and J. Villasenor
-  * IEEE Trans. on Image Processing, Vol. 6, No 8, Aug. 1997
-  * Page 1170, formula (7) and corresponding Table IV
-  * Table IV has 2 entries for Cb and Cr thresholds
-  * Chose those corresponding to subject "sfl" since they are lower
-  * These thresholds were obtained and modeled for the 7-9 biorthogonal wavelet basis
-  */
-struct dwt_model_params {
-    float a;
-    float k;
-    float f0;
-    float g[4];
-};
-
-// 0 -> Y, 1 -> Cb, 2 -> Cr
-static const struct dwt_model_params dwt_7_9_YCbCr_threshold[3] = {
-    {.a = 0.495,.k = 0.466,.f0 = 0.401,.g = { 1.501, 1.0, 0.534, 1.0} },
-    {.a = 1.633,.k = 0.353,.f0 = 0.209,.g = { 1.520, 1.0, 0.502, 1.0} },
-    {.a = 0.944,.k = 0.521,.f0 = 0.404,.g = { 1.868, 1.0, 0.516, 1.0} }
-};
-
-/*
- * The following dwt basis function amplitudes, A(lambda,theta), are taken from
- * "Visibility of Wavelet Quantization Noise"
- * by A. B. Watson, G. Y. Yang, J. A. Solomon and J. Villasenor
- * IEEE Trans. on Image Processing, Vol. 6, No 8, Aug. 1997
- * Page 1172, Table V
- * The table has been transposed, i.e. it can be used directly to obtain A[lambda][theta]
- * These amplitudes were calculated for the 7-9 biorthogonal wavelet basis
- */
-static const float dwt_7_9_basis_function_amplitudes[6][4] = {
-    { 0.62171,  0.67234,  0.72709,  0.67234  },
-    { 0.34537,  0.41317,  0.49428,  0.41317  },
-    { 0.18004,  0.22727,  0.28688,  0.22727  },
-    { 0.091401, 0.11792,  0.15214,  0.11792  },
-    { 0.045943, 0.059758, 0.077727, 0.059758 },
-    { 0.023013, 0.030018, 0.039156, 0.030018 }
-};
 
 /*
  * lambda = 0 (finest scale), 1, 2, 3 (coarsest scale);
@@ -1194,8 +1058,8 @@ static float adm_csf_den_scale(const adm_dwt_band_t *src, int w, int h,
             accum_inner_d += val;
         }
         /**
-         * max_value of h^3, v^3, d^3 is 1.205624776×10^13
-         * accum_h can hold till 1.844674407×10^19
+         * max_value of h^3, v^3, d^3 is 1.205624776 * —10^13
+         * accum_h can hold till 1.844674407 * —10^19
          * accum_h's maximum is reached when it is 2^20 * max(h^3)
          * Therefore the accum_h,v,d is shifted based on width and height subtracted by 20
          */
@@ -1374,12 +1238,12 @@ static float adm_cm(AdmBuffer *buf, int w, int h, int src_stride, int csf_a_stri
         /**
          * max value of xh_sq and xv_sq is 1301381973 and that of xd_sq is 1195806729
          *
-         * max(val before shift for h and v) is 9.995357299×10^17.
-         * 9.995357299×10^17 * 2^4 is close to 2^64.
+         * max(val before shift for h and v) is 9.995357299 * —10^17.
+         * 9.995357299 * —10^17 * 2^4 is close to 2^64.
          * Hence shift is done based on width subtracting 4
          *
-         * max(val before shift for d) is 1.355006643×10^18
-         * 1.355006643×10^18 * 2^3 is close to 2^64
+         * max(val before shift for d) is 1.355006643 * —10^18
+         * 1.355006643 * —10^18 * 2^3 is close to 2^64
          * Hence shift is done based on width subtracting 3
          */
         ADM_CM_ACCUM_ROUND(xh, thr, shift_xhsub, xh_sq, add_shift_xhsq, shift_xhsq, val,
@@ -2080,8 +1944,9 @@ static void i16_to_i32(adm_dwt_band_t *src, i4_adm_dwt_band_t *dst,
     }
 }
 
-static void adm_dwt2_8(const uint8_t *src, const adm_dwt_band_t *dst, AdmBuffer *buf, int w, int h,
-                       int src_stride, int dst_stride)
+static void adm_dwt2_8(const uint8_t *src, const adm_dwt_band_t *dst,
+                       AdmBuffer *buf, int w, int h, int src_stride,
+                       int dst_stride)
 {
     const int16_t *filter_lo = dwt2_db2_coeffs_lo;
     const int16_t *filter_hi = dwt2_db2_coeffs_hi;
@@ -2437,8 +2302,8 @@ static void adm_dwt2_s123_combined(const int32_t *i4_ref_scale, const int32_t *i
     }
 }
 
-void integer_compute_adm(VmafPicture *ref_pic, VmafPicture *dis_pic, double *score,
-                         double *scores, AdmBuffer *buf)
+void integer_compute_adm(AdmState *s, VmafPicture *ref_pic, VmafPicture *dis_pic,
+                         double *score, double *scores, AdmBuffer *buf)
 {
     int w = ref_pic->w[0];
     int h = ref_pic->h[0];
@@ -2470,10 +2335,10 @@ void integer_compute_adm(VmafPicture *ref_pic, VmafPicture *dis_pic, double *sco
         dwt2_src_indices_filt(buf->ind_y, buf->ind_x, w, h);
 		if(scale==0) {
             if (ref_pic->bpc == 8) {
-                adm_dwt2_8(ref_pic->data[0], &buf->ref_dwt2, buf, w, h,
-                           curr_ref_stride, buf_stride);
-                adm_dwt2_8(dis_pic->data[0], &buf->dis_dwt2, buf, w, h,
-                           curr_dis_stride, buf_stride);
+                s->dwt2_8(ref_pic->data[0], &buf->ref_dwt2, buf, w, h,
+                          curr_ref_stride, buf_stride);
+                s->dwt2_8(dis_pic->data[0], &buf->dis_dwt2, buf, w, h,
+                          curr_dis_stride, buf_stride);
             }
             else {
                 adm_dwt2_16(ref_pic->data[0], &buf->ref_dwt2, buf, w, h,
@@ -2588,6 +2453,15 @@ static int init(VmafFeatureExtractor *fex, enum VmafPixelFormat pix_fmt,
     (void) pix_fmt;
     (void) bpc;
 
+    s->dwt2_8 = adm_dwt2_8;
+
+#if ARCH_X86
+    unsigned flags = vmaf_get_cpu_flags();
+    if (flags & VMAF_X86_CPU_FLAG_AVX2) {
+        s->dwt2_8 = adm_dwt2_8_avx2;
+    }
+#endif
+
     s->integer_stride   = ALIGN_CEIL(w * sizeof(int32_t));
     s->buf.ind_size_x   = ALIGN_CEIL(((w + 1) / 2) * sizeof(int32_t));
     s->buf.ind_size_y   = ALIGN_CEIL(((h + 1) / 2) * sizeof(int32_t));
@@ -2649,7 +2523,7 @@ static int extract(VmafFeatureExtractor *fex,
     double score;
     double scores[8];
 
-    integer_compute_adm(ref_pic, dist_pic, &score, scores, &s->buf);
+    integer_compute_adm(s, ref_pic, dist_pic, &score, scores, &s->buf);
 
     err |= vmaf_feature_collector_append(feature_collector,
                                         "'VMAF_feature_adm2_integer_score'",
