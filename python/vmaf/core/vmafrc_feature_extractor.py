89ec9d67cec9dd56e0af8863c8911815b6318c95
Zhi Li <zli@netflix.com>
2020-11-16 14:09:12 -0800

    Update version number of integer motion/VIF fex after vectorization.

--- python/vmaf/core/vmafrc_feature_extractor.py
+++ python/vmaf/core/vmafrc_feature_extractor.py
@@ -34,7 +34,8 @@ class FloatMotionFeatureExtractor(VmafrcFeatureExtractorMixin, FeatureExtractor)
 class IntegerMotionFeatureExtractor(VmafrcFeatureExtractorMixin, FeatureExtractor):
 
     TYPE = "integer_motion_feature"
-    VERSION = "1.0"
+    # VERSION = "1.0"
+    VERSION = "1.1"  # vectorization
 
     ATOM_FEATURES = ['motion2']
 
@@ -96,7 +97,8 @@ class FloatVifFeatureExtractor(VmafrcFeatureExtractorMixin, FeatureExtractor):
 class IntegerVifFeatureExtractor(VmafrcFeatureExtractorMixin, FeatureExtractor):
 
     TYPE = "integer_VIF_feature"
-    VERSION = "1.0"
+    # VERSION = "1.0"
+    VERSION = "1.1"  # vectorization
 
     ATOM_FEATURES = ['vif_scale0', 'vif_scale1', 'vif_scale2', 'vif_scale3',
                      ]
