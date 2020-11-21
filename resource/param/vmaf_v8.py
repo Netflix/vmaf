245fe3c17dcbc516ecc29d406e9ff26d22dd0443
Zhi Li <zli@netflix.com>
2020-11-21 14:24:34 -0800

    Update vmaf_v8 training param script.

--- resource/param/vmaf_v8.py
+++ resource/param/vmaf_v8.py
@@ -1,6 +1,6 @@
 feature_dict = {
 
-   'VMAF_feature': ['vif_scale0', 'vif_scale1', 'vif_scale2', 'vif_scale3',
+   'VMAF_integer_feature': ['vif_scale0', 'vif_scale1', 'vif_scale2', 'vif_scale3',
                     'adm2', 'motion2',],
 
 }
