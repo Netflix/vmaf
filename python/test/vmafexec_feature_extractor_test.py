03cd5f75d8bb9e39a71d7d9317ea26595b480723
Zhi Li <zli@netflix.com>
2020-12-06 11:31:03 -0800

    Remove PSNR-HVS code.

--- python/test/vmafexec_feature_extractor_test.py
+++ python/test/vmafexec_feature_extractor_test.py
@@ -6,7 +6,7 @@ from vmaf.core.asset import Asset
 from vmaf.config import VmafConfig
 from vmaf.core.vmafexec_feature_extractor import FloatMotionFeatureExtractor, IntegerMotionFeatureExtractor, \
     FloatVifFeatureExtractor, FloatAdmFeatureExtractor, IntegerVifFeatureExtractor, IntegerPsnrFeatureExtractor, \
-    IntegerAdmFeatureExtractor, PsnrhvsFeatureExtractor, CIEDE2000FeatureExtractor
+    IntegerAdmFeatureExtractor, CIEDE2000FeatureExtractor
 from test.testutil import set_default_576_324_videos_for_testing, \
     set_default_576_324_12bit_videos_for_testing, \
     set_default_576_324_16bit_videos_for_testing, \
@@ -774,32 +774,6 @@ class FeatureExtractorTest(unittest.TestCase):
         self.fextractor.run(parallelize=True)
         results = self.fextractor.results
         self.assertAlmostEqual(results[0]['integer_ADM_feature_adm2_score'], 1.116609, places=6)  # float 1.116595
-
-    def test_run_psnrhvs_fextractor(self):
-        ref_path, dis_path, asset, asset_original = set_default_576_324_videos_for_testing()
-        self.fextractor = PsnrhvsFeatureExtractor(
-            [asset, asset_original],
-            None, fifo_mode=False,
-            result_store=None
-        )
-        self.fextractor.run(parallelize=True)
-        results = self.fextractor.results
-        try: self.assertAlmostEqual(results[0]['PSNRHVS_feature_psnr_hvs_score'], 31.33044560416667, places=4)
-        except AssertionError as e: self.verificationErrors.append(str(e))
-        try: self.assertAlmostEqual(results[0]['PSNRHVS_feature_psnr_hvs_y_score'], 30.578766166666668, places=4)
-        except AssertionError as e: self.verificationErrors.append(str(e))
-        try: self.assertAlmostEqual(results[0]['PSNRHVS_feature_psnr_hvs_cb_score'], 37.25849772916666, places=4)
-        except AssertionError as e: self.verificationErrors.append(str(e))
-        try: self.assertAlmostEqual(results[0]['PSNRHVS_feature_psnr_hvs_cr_score'], 38.2002601875, places=4)
-        except AssertionError as e: self.verificationErrors.append(str(e))
-        try: self.assertAlmostEqual(results[1]['PSNRHVS_feature_psnr_hvs_score'], np.float('inf'), places=4)
-        except AssertionError as e: self.verificationErrors.append(str(e))
-        try: self.assertAlmostEqual(results[1]['PSNRHVS_feature_psnr_hvs_y_score'], np.float('inf'), places=4)
-        except AssertionError as e: self.verificationErrors.append(str(e))
-        try: self.assertAlmostEqual(results[1]['PSNRHVS_feature_psnr_hvs_cb_score'], np.float('inf'), places=4)
-        except AssertionError as e: self.verificationErrors.append(str(e))
-        try: self.assertAlmostEqual(results[1]['PSNRHVS_feature_psnr_hvs_cr_score'], np.float('inf'), places=4)
-        except AssertionError as e: self.verificationErrors.append(str(e))
 
     def test_run_ciede2000_fextractor(self):
         ref_path, dis_path, asset, asset_original = set_default_576_324_videos_for_testing()
