5b8a9c1ca8ad5a74f0c56c771f9f9570b9143133
Kyle Swanson <kswanson@netflix.com>
2021-01-13 17:12:14 -0800

    libvmaf/psnr: add min_sse option

--- libvmaf/src/feature/integer_psnr.c
+++ libvmaf/src/feature/integer_psnr.c
@@ -17,6 +17,7 @@
  */
 
 #include <errno.h>
+#include <float.h>
 #include <math.h>
 #include <stddef.h>
 #include <string.h>
@@ -31,6 +32,8 @@ typedef struct PsnrState {
     bool enable_apsnr;
     bool reduced_hbd_peak;
     uint32_t peak;
+    double psnr_max[3];
+    double min_sse;
     struct {
         uint64_t sse[3];
         uint64_t n_pixels[3];
@@ -66,6 +69,15 @@ static const VmafOption options[] = {
         .type = VMAF_OPT_TYPE_BOOL,
         .default_val.b = false,
     },
+    {
+        .name = "min_sse",
+        .help = "constrain the minimum possible sse",
+        .offset = offsetof(PsnrState, min_sse),
+        .type = VMAF_OPT_TYPE_DOUBLE,
+        .default_val.d = 0.0,
+        .min = 0.0,
+        .max = DBL_MAX,
+    },
     { 0 }
 };
 
@@ -73,13 +85,21 @@ static const VmafOption options[] = {
 static int init(VmafFeatureExtractor *fex, enum VmafPixelFormat pix_fmt,
                 unsigned bpc, unsigned w, unsigned h)
 {
-    (void) pix_fmt;
-    (void) bpc;
-    (void) w;
-    (void) h;
-
     PsnrState *s = fex->priv;
     s->peak = s->reduced_hbd_peak ? 255 * 1 << (bpc - 8) : (1 << bpc) - 1;
+
+    for (unsigned i = 0; i < 3; i++) {
+        if (s->min_sse != 0.0) {
+            const int ss_hor = pix_fmt != VMAF_PIX_FMT_YUV444P;
+            const int ss_ver = pix_fmt == VMAF_PIX_FMT_YUV420P;
+            const double mse = s->min_sse / 
+                (((i && ss_hor) ? w / 2 : w) * ((i && ss_ver) ? h / 2 : h));
+            s->psnr_max[i] = ceil(10. * log10(s->peak * s->peak / mse));
+        } else {
+            s->psnr_max[i] = (6 * bpc) + 12;
+        }
+    }
+
     return 0;
 }
 
@@ -94,7 +114,6 @@ static int psnr(VmafPicture *ref_pic, VmafPicture *dist_pic,
                 PsnrState *s)
 {
     const uint8_t peak = 255; // (1 << ref_pic->bpc) - 1;
-    const double psnr_max = 60.; // (6 * ref_pic->bpc) + 12;
     const unsigned n = s->enable_chroma ? 3 : 1;
 
     int err = 0;
@@ -122,7 +141,7 @@ static int psnr(VmafPicture *ref_pic, VmafPicture *dist_pic,
 
         const double mse = ((double) sse) / (ref_pic->w[p] * ref_pic->h[p]);
         const double psnr =
-            MIN(10. * log10(peak * peak / MAX(mse, 1e-16)), psnr_max);
+            MIN(10. * log10(peak * peak / MAX(mse, 1e-16)), s->psnr_max[p]);
 
         err |= vmaf_feature_collector_append(feature_collector, psnr_name[p],
                                              psnr, index);
@@ -139,7 +158,6 @@ static int psnr_hbd(VmafPicture *ref_pic, VmafPicture *dist_pic,
                     unsigned index, VmafFeatureCollector *feature_collector,
                     PsnrState *s)
 {
-    const double psnr_max = (6 * ref_pic->bpc) + 12;
     const unsigned n = s->enable_chroma ? 3 : 1;
 
     int err = 0;
@@ -165,7 +183,8 @@ static int psnr_hbd(VmafPicture *ref_pic, VmafPicture *dist_pic,
 
         const double mse = ((double) sse) / (ref_pic->w[p] * ref_pic->h[p]);
         const double psnr =
-            MIN(10. * log10(s->peak * s->peak / MAX(mse, 1e-16)), psnr_max);
+            MIN(10. * log10(s->peak * s->peak / MAX(mse, 1e-16)),
+                s->psnr_max[p]);
 
         err |= vmaf_feature_collector_append(feature_collector, psnr_name[p],
                                              psnr, index);
@@ -209,12 +228,20 @@ static int flush(VmafFeatureExtractor *fex,
     int err = 0;
     if (s->enable_apsnr) {
         for (unsigned i = 0; i < 3; i++) {
+
             double apsnr = 10 * (log10(s->peak * s->peak) +
                                  log10(s->apsnr.n_pixels[i]) -
                                  log10(s->apsnr.sse[i]));
+
+            double max_apsnr =
+                ceil(10 * log10(s->peak * s->peak *
+                                s->apsnr.n_pixels[i] *
+                                2));
+
             err |=
                 vmaf_feature_collector_set_aggregate(feature_collector,
-                                                     apsnr_name[i], apsnr);
+                                                     apsnr_name[i],
+                                                     MIN(apsnr, max_apsnr));
         }
     }
 
