abeebfaa80786521529c67ed94e723ee2e0bf605
nilfm <nfonsmiret@netflix.com>
2022-03-14 11:21:38 -0700

    Convert tabs to spaces and reduce diff size

--- libvmaf/src/feature/x86/vif_avx2.h
+++ libvmaf/src/feature/x86/vif_avx2.h
@@ -32,8 +32,8 @@ void vif_subsample_rd_16_avx2(VifBuffer buf, unsigned w, unsigned h, int scale,
 
 void vif_filter1d_16_avx2(VifBuffer buf, unsigned w, unsigned h, int scale, int bpc);
 
-void vif_statistic_8_avx2(struct VifState* s, float* num, float* den, unsigned w, unsigned h);
+void vif_statistic_8_avx2(struct VifPublicState *s, float *num, float *den, unsigned w, unsigned h);
 
-void vif_statistic_16_avx2(struct VifState* s, float* num, float* den, unsigned w, unsigned h, int bpc, int scale);
+void vif_statistic_16_avx2(struct VifPublicState *s, float *num, float *den, unsigned w, unsigned h, int bpc, int scale);
 
 #endif /* X86_AVX2_VIF_H_ */
