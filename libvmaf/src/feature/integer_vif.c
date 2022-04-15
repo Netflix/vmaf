4a5bf3f3744d018c5662945b85a39a9612bfc047
Kyle Swanson <kswanson@netflix.com>
2022-04-15 13:37:43 -0700

    fix pointer type mismatch warning

--- libvmaf/src/feature/integer_vif.c
+++ libvmaf/src/feature/integer_vif.c
@@ -631,7 +631,7 @@ static int init(VmafFeatureExtractor *fex, enum VmafPixelFormat pix_fmt,
     const size_t data_sz =
         2 * (pad_size + frame_size + pad_size) + 2 * (h * s->public.buf.stride_16) +
         5 * (s->public.buf.stride_32) + 7 * s->public.buf.stride_tmp;
-    unsigned char *data = aligned_malloc(data_sz, MAX_ALIGN);
+    void *data = aligned_malloc(data_sz, MAX_ALIGN);
     if (!data) return -ENOMEM;
 
     s->public.buf.data = data; data += pad_size;
