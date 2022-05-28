0beed3d63d104d3b0791131306eea523b9962e79
Zhi Li <zli@netflix.com>
2022-05-28 10:03:46 -0700

    Add 10, 12, 16 bit tests.

--- python/test/third_party/xiph/vmafexec_feature_extractor_test.py
+++ python/test/third_party/xiph/vmafexec_feature_extractor_test.py
@@ -2,7 +2,10 @@ import unittest
 
 import numpy as np
 
-from test.testutil import set_default_576_324_videos_for_testing
+from test.testutil import set_default_576_324_videos_for_testing, \
+    set_default_576_324_16bit_videos_for_testing, \
+    set_default_576_324_12bit_videos_for_testing, \
+    set_default_576_324_10bit_videos_for_testing_b
 from vmaf.third_party.xiph.vmafexec_feature_extractor import PsnrhvsFeatureExtractor
 from vmaf.tools.misc import MyTestCase
 
@@ -30,6 +33,60 @@ class FeatureExtractorTest(MyTestCase):
         self.assertAlmostEqual(results[0]['PSNRHVS_feature_psnr_hvs_y_score'], 30.578766166666668, places=4)
         self.assertAlmostEqual(results[0]['PSNRHVS_feature_psnr_hvs_cb_score'], 37.25849772916666, places=4)
         self.assertAlmostEqual(results[0]['PSNRHVS_feature_psnr_hvs_cr_score'], 38.2002601875, places=4)
+        self.assertAlmostEqual(results[1]['PSNRHVS_feature_psnr_hvs_score'], float('inf'), places=4)
+        self.assertAlmostEqual(results[1]['PSNRHVS_feature_psnr_hvs_y_score'], float('inf'), places=4)
+        self.assertAlmostEqual(results[1]['PSNRHVS_feature_psnr_hvs_cb_score'], float('inf'), places=4)
+        self.assertAlmostEqual(results[1]['PSNRHVS_feature_psnr_hvs_cr_score'], float('inf'), places=4)
+
+    def test_run_psnrhvs_fextractor_10bit_b(self):
+        ref_path, dis_path, asset, asset_original = set_default_576_324_10bit_videos_for_testing_b()
+        self.fextractor = PsnrhvsFeatureExtractor(
+            [asset, asset_original],
+            None, fifo_mode=False,
+            result_store=None
+        )
+        self.fextractor.run(parallelize=True)
+        results = self.fextractor.results
+        self.assertAlmostEqual(results[0]['PSNRHVS_feature_psnr_hvs_score'], 33.937514666666665, places=4)
+        self.assertAlmostEqual(results[0]['PSNRHVS_feature_psnr_hvs_y_score'], 33.33393133333333, places=4)
+        self.assertAlmostEqual(results[0]['PSNRHVS_feature_psnr_hvs_cb_score'], 38.065045999999995, places=4)
+        self.assertAlmostEqual(results[0]['PSNRHVS_feature_psnr_hvs_cr_score'], 38.93428866666667, places=4)
+        self.assertAlmostEqual(results[1]['PSNRHVS_feature_psnr_hvs_score'], float('inf'), places=4)
+        self.assertAlmostEqual(results[1]['PSNRHVS_feature_psnr_hvs_y_score'], float('inf'), places=4)
+        self.assertAlmostEqual(results[1]['PSNRHVS_feature_psnr_hvs_cb_score'], float('inf'), places=4)
+        self.assertAlmostEqual(results[1]['PSNRHVS_feature_psnr_hvs_cr_score'], float('inf'), places=4)
+
+    def test_run_psnrhvs_fextractor_12bit(self):
+        ref_path, dis_path, asset, asset_original = set_default_576_324_12bit_videos_for_testing()
+        self.fextractor = PsnrhvsFeatureExtractor(
+            [asset, asset_original],
+            None, fifo_mode=False,
+            result_store=None
+        )
+        self.fextractor.run(parallelize=True)
+        results = self.fextractor.results
+        self.assertAlmostEqual(results[0]['PSNRHVS_feature_psnr_hvs_score'], 33.95532633333334, places=4)
+        self.assertAlmostEqual(results[0]['PSNRHVS_feature_psnr_hvs_y_score'], 33.350897, places=4)
+        self.assertAlmostEqual(results[0]['PSNRHVS_feature_psnr_hvs_cb_score'], 38.088281333333335, places=4)
+        self.assertAlmostEqual(results[0]['PSNRHVS_feature_psnr_hvs_cr_score'], 38.97104833333333, places=4)
+        self.assertAlmostEqual(results[1]['PSNRHVS_feature_psnr_hvs_score'], float('inf'), places=4)
+        self.assertAlmostEqual(results[1]['PSNRHVS_feature_psnr_hvs_y_score'], float('inf'), places=4)
+        self.assertAlmostEqual(results[1]['PSNRHVS_feature_psnr_hvs_cb_score'], float('inf'), places=4)
+        self.assertAlmostEqual(results[1]['PSNRHVS_feature_psnr_hvs_cr_score'], float('inf'), places=4)
+
+    def test_run_psnrhvs_fextractor_16bit(self):
+        ref_path, dis_path, asset, asset_original = set_default_576_324_16bit_videos_for_testing()
+        self.fextractor = PsnrhvsFeatureExtractor(
+            [asset, asset_original],
+            None, fifo_mode=False,
+            result_store=None
+        )
+        self.fextractor.run(parallelize=True)
+        results = self.fextractor.results
+        self.assertAlmostEqual(results[0]['PSNRHVS_feature_psnr_hvs_score'], 33.95532633333334, places=4)
+        self.assertAlmostEqual(results[0]['PSNRHVS_feature_psnr_hvs_y_score'], 33.350897, places=4)
+        self.assertAlmostEqual(results[0]['PSNRHVS_feature_psnr_hvs_cb_score'], 38.088281333333335, places=4)
+        self.assertAlmostEqual(results[0]['PSNRHVS_feature_psnr_hvs_cr_score'], 38.97104833333333, places=4)
         self.assertAlmostEqual(results[1]['PSNRHVS_feature_psnr_hvs_score'], float('inf'), places=4)
         self.assertAlmostEqual(results[1]['PSNRHVS_feature_psnr_hvs_y_score'], float('inf'), places=4)
         self.assertAlmostEqual(results[1]['PSNRHVS_feature_psnr_hvs_cb_score'], float('inf'), places=4)
