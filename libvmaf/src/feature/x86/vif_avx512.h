8221b68fcb6d4c7a6bc56ab57b3bba493ebd26a6
nilfm <nilf@netflix.com>
2022-12-26 09:32:43 +0000

    integer_vif: fix avx512 function usage

--- libvmaf/src/feature/x86/vif_avx512.h
+++ libvmaf/src/feature/x86/vif_avx512.h
@@ -21,7 +21,7 @@
 
 #include "feature/integer_vif.h"
 
-void vif_subsample_rd_8_avx2(VifBuffer buf, unsigned w, unsigned h);
+void vif_subsample_rd_8_avx512(VifBuffer buf, unsigned w, unsigned h);
 
 void vif_subsample_rd_16_avx512(VifBuffer buf, unsigned w, unsigned h, int scale,
                              int bpc);
