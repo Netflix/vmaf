2dfd5ca89d287e2e8c3848232d375b581a8dc0ff
Christos Bampis <christosb@netflix.com>
2023-08-20 19:16:39 -0700

    Add remove option utility.

--- python/vmaf/tools/testutils.py
+++ python/vmaf/tools/testutils.py
@@ -49,7 +49,54 @@ def remove_redundant_whitespace(command_line: str) -> str:
     return " ".join(command_line.split())
 
 
-def assert_equivalent_commands(self, cmds: List[str], cmds_expected: List[str], root: str, root_expected: str, do_replace_uuid: bool = True):
+def remove_option(command_line: str, option: str) -> str:
+    """
+    Removes a whitespace-separated option that is prefixed by two dashes, e.g., --option_name.
+    >>> remove_option('vmaf --reference REFERENCE --model MODEL', 'model')
+    'vmaf --reference REFERENCE'
+    >>> remove_option('vmaf --model MODEL --reference REFERENCE', 'model')
+    'vmaf --reference REFERENCE'
+    >>> remove_option(remove_option('vmaf --model MODEL --dist DIST --reference REFERENCE', 'model'), 'reference')
+    'vmaf --dist DIST'
+    >>> remove_option(remove_option('vmaf --model MODEL --dist DIST --reference REFERENCE', 'reference'), 'model')
+    'vmaf --dist DIST'
+    >>> remove_option('vmaf --reference REFERENCE', 'model')
+    'vmaf --reference REFERENCE'
+    >>> remove_option('a --model M b', 'model')
+    'a b'
+    >>> remove_option('a --model KLM b', 'model')
+    'a b'
+    >>> remove_option('abc --model K def', 'model')
+    'abc def'
+    >>> remove_option('abc --model KLM def', 'model')
+    'abc def'
+    >>> remove_option('abc --model KLM d', 'model')
+    'abc d'
+    >>> remove_option('a --model KLM def', 'model')
+    'a def'
+    >>> remove_option('a --model M b c --model M d', 'model')
+    'a b c d'
+    >>> remove_option('a --model M', 'model')
+    'a'
+    >>> remove_option('a --model M ', 'model')
+    'a '
+    >>> remove_option(' --model M ', 'model')
+    ' '
+    >>> remove_option('--model M ', 'model')
+    ' '
+    >>> remove_option('--model2 M ', 'model')
+    '--model2 M '
+    >>> remove_option('-- model M ', 'model')
+    '-- model M '
+    """
+    if command_line.startswith('--{option}'.format(option=option)):
+        return re.sub(r'--{option} [^\s]*'.format(option=option), '', command_line)
+    else:
+        return re.sub(r' --{option} [^\s]*'.format(option=option), '', command_line)
+
+
+def assert_equivalent_commands(self, cmds: List[str], cmds_expected: List[str], root: str, root_expected: str, do_replace_uuid: bool = True,
+                               options_to_remove=None):
     """
     >>> self = MyTestCase()
     >>> self.setUp()
@@ -59,8 +106,18 @@ def assert_equivalent_commands(self, cmds: List[str], cmds_expected: List[str], 
     >>> self2.setUp()
     >>> assert_equivalent_commands(self2, cmds=["/opt/project/vmaf/libvmaf/build/tools/vmaf /tmp/72b3a7af-204c-4455-afe5-be2d536f2fdd/dv_el_out_1.h265"], cmds_expected=["/opt/project/vmaf/libvmaf/build/tools/vmaf /tmp/82b3a7af-304c-5455-afe5-be2d536f2fdd/dv_el_out_1.h265"], root="/opt/project", root_expected="/opt/project", do_replace_uuid=False)
     >>> with self.assertRaises(AssertionError): self2.tearDown()
+    >>> self3 = MyTestCase()
+    >>> self3.setUp()
+    >>> assert_equivalent_commands(self, cmds=["/opt/project/vmaf --reference ref.h265 --distorted dist.h265"], cmds_expected=["/opt/project/vmaf --reference ref.h265"], root="/opt/project", root_expected="/opt/project", options_to_remove=["distorted"])
+    >>> self3.tearDown()
+    >>> self3 = MyTestCase()
+    >>> self3.setUp()
+    >>> assert_equivalent_commands(self, cmds=["/opt/project/vmaf --reference ref.h265 --distorted dist.h265 --output output.xml"], cmds_expected=["/opt/project/vmaf  --output output.xml"], root="/opt/project", root_expected="/opt/project", options_to_remove=["distorted", "reference"])
+    >>> self3.tearDown()
     """
 
+    if options_to_remove is None:
+        options_to_remove = []
     assert len(cmds) == len(cmds_expected), f"length of cmds and cmds_expected are not equal: {len(cmds)} vs. {len(cmds_expected)}"
     for cmd, cmd_expected in zip(cmds, cmds_expected):
 
@@ -70,9 +127,13 @@ def assert_equivalent_commands(self, cmds: List[str], cmds_expected: List[str], 
             cmd1 = cmd
         cmd2 = replace_root(cmd1, root)
         cmd3 = remove_redundant_whitespace(cmd2)
+        for option_to_remove in options_to_remove:
+            cmd3 = remove_option(cmd3, option_to_remove)
 
         cmd_expected1 = replace_uuid(cmd_expected)
         cmd_expected2 = replace_root(cmd_expected1, root_expected)
         cmd_expected3 = remove_redundant_whitespace(cmd_expected2)
+        for option_to_remove in options_to_remove:
+            cmd_expected3 = remove_option(cmd_expected3, option_to_remove)
 
         self.assertEqual(cmd3, cmd_expected3, msg=f"cmd and cmd_expected are not matched:\ncmd: {cmd}\ncmd:expected: {cmd_expected}")
