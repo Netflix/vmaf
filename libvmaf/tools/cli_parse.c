ce48de9a9b4e8a41cbeb4abbc9b0ac528870d75c
Kyle Swanson <kswanson@netflix.com>
2026-03-31 10:41:09 -0700

    tools/cli_parse: add --aom_ctc v7.0

--- libvmaf/tools/cli_parse.c
+++ libvmaf/tools/cli_parse.c
@@ -356,6 +356,15 @@ static void aom_ctc_v6_0(CLISettings *settings, const char *app)
     settings->common_bitdepth = true;
 }
 
+static void aom_ctc_v7_0(CLISettings *settings, const char *app)
+{
+    aom_ctc_v6_0(settings, app);
+    CHECKED_REPLACE(settings->feature_cfg, settings->feature_cnt,
+        parse_feature_config("float_ssim=scale=1:enable_db=true:clip_db=true",
+                             app),
+        app, "features");
+}
+
 static void parse_aom_ctc(CLISettings *settings, const char *const optarg,
                           const char *const app)
 {
@@ -389,6 +398,11 @@ static void parse_aom_ctc(CLISettings *settings, const char *const optarg,
 
     if (!strcmp(optarg, "v6.0")) {
         aom_ctc_v6_0(settings, app);
+        return;
+    }
+
+    if (!strcmp(optarg, "v7.0")) {
+        aom_ctc_v7_0(settings, app);
         return;
     }
 
