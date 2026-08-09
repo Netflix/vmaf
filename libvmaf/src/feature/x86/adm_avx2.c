e84acb70fabec27ee9f47e49ea46c409cce58942
David C. Manuelda <StormByte@gmail.com>
2026-08-09 05:31:09 +0200

    AVX2/AVX512: portable horizontal epi64 sum for MSVC/i386
    Replace non-portable __m128i lane indexing (v[0]+v[1]) with
    mm_hadd_epi64(): extract on x86-64, store on 32-bit.
    
    Signed-off-by: David C. Manuelda <StormByte@gmail.com>

--- libvmaf/src/feature/x86/adm_avx2.c
+++ libvmaf/src/feature/x86/adm_avx2.c
@@ -25,6 +25,17 @@
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 
+static inline int64_t mm_hadd_epi64(__m128i v)
+{
+#if defined(__x86_64__) || defined(_M_X64)
+    return (int64_t)_mm_extract_epi64(v, 0) + (int64_t)_mm_extract_epi64(v, 1);
+#else
+    int64_t lanes[2];
+    _mm_storeu_si128((__m128i *)lanes, v);
+    return lanes[0] + lanes[1];
+#endif
+}
+
 #define shift15_64b_signExt_256(a, r) \
 { \
     r = _mm256_add_epi64( _mm256_srli_epi64(a, 15) , _mm256_and_si256(a, _mm256_set1_epi64x(0xFFFE000000000000))); \
@@ -2152,15 +2163,15 @@ float adm_cm_avx2(AdmBuffer *buf, int w, int h, int src_stride, int csf_a_stride
             }
             accum_inner_h_lo_256 = _mm256_add_epi64(accum_inner_h_lo_256, accum_inner_h_hi_256);
             __m128i r2_h = _mm_add_epi64(_mm256_castsi256_si128(accum_inner_h_lo_256), _mm256_extracti128_si256(accum_inner_h_lo_256, 1));
-            int64_t res_h = r2_h[0] + r2_h[1];
+            int64_t res_h = mm_hadd_epi64(r2_h);
 
             accum_inner_v_lo_256 = _mm256_add_epi64(accum_inner_v_lo_256, accum_inner_v_hi_256);
             __m128i r2_v = _mm_add_epi64(_mm256_castsi256_si128(accum_inner_v_lo_256), _mm256_extracti128_si256(accum_inner_v_lo_256, 1));
-            int64_t res_v = r2_v[0] + r2_v[1];
+            int64_t res_v = mm_hadd_epi64(r2_v);
 
             accum_inner_d_lo_256 = _mm256_add_epi64(accum_inner_d_lo_256, accum_inner_d_hi_256);
             __m128i r2_d = _mm_add_epi64(_mm256_castsi256_si128(accum_inner_d_lo_256), _mm256_extracti128_si256(accum_inner_d_lo_256, 1));
-            int64_t res_d = r2_d[0] + r2_d[1];
+            int64_t res_d = mm_hadd_epi64(r2_d);
 
             for (j = end_col_mod6; j < end_col; ++j) {
                 xh = src->band_h[i * src_stride + j] * i_rfactor[0];
@@ -2610,13 +2621,13 @@ float i4_adm_cm_avx2(AdmBuffer *buf, int w, int h, int src_stride, int csf_a_str
             }
 
             __m128i r2_h = _mm_add_epi64(_mm256_castsi256_si128(accum_inner_h_256), _mm256_extracti128_si256(accum_inner_h_256, 1));
-            int64_t res_h = r2_h[0] + r2_h[1];
+            int64_t res_h = mm_hadd_epi64(r2_h);
 
             __m128i r2_v = _mm_add_epi64(_mm256_castsi256_si128(accum_inner_v_256), _mm256_extracti128_si256(accum_inner_v_256, 1));
-            int64_t res_v = r2_v[0] + r2_v[1];
+            int64_t res_v = mm_hadd_epi64(r2_v);
 
             __m128i r2_d = _mm_add_epi64(_mm256_castsi256_si128(accum_inner_d_256), _mm256_extracti128_si256(accum_inner_d_256, 1));
-            int64_t res_d = r2_d[0] + r2_d[1];
+            int64_t res_d = mm_hadd_epi64(r2_d);
 
             for (j = end_col_mod2; j < end_col; ++j)
             {
@@ -3689,15 +3700,15 @@ float adm_csf_den_scale_avx2(const adm_dwt_band_t *src, int w, int h,
 
         accum_inner_h_lo = _mm256_add_epi64(accum_inner_h_lo, accum_inner_h_hi);
         __m128i h_r2 = _mm_add_epi64(_mm256_castsi256_si128(accum_inner_h_lo), _mm256_extracti128_si256(accum_inner_h_lo, 1));
-        uint64_t h_r1 = h_r2[0] + h_r2[1];
+        uint64_t h_r1 = mm_hadd_epi64(h_r2);
 
         accum_inner_v_lo = _mm256_add_epi64(accum_inner_v_lo, accum_inner_v_hi);
         __m128i v_r2 = _mm_add_epi64(_mm256_castsi256_si128(accum_inner_v_lo), _mm256_extracti128_si256(accum_inner_v_lo, 1));
-        uint64_t v_r1 = v_r2[0] + v_r2[1];
+        uint64_t v_r1 = mm_hadd_epi64(v_r2);
 
         accum_inner_d_lo = _mm256_add_epi64(accum_inner_d_lo, accum_inner_d_hi);
         __m128i d_r2 = _mm_add_epi64(_mm256_castsi256_si128(accum_inner_d_lo), _mm256_extracti128_si256(accum_inner_d_lo, 1));
-        uint64_t d_r1 = d_r2[0] + d_r2[1];
+        uint64_t d_r1 = mm_hadd_epi64(d_r2);
 
         for (int j = right_mod_8; j < right; ++j) {
             uint16_t h = (uint16_t)abs(src_h[j]);
@@ -4157,13 +4168,13 @@ float adm_csf_den_s123_avx2(const i4_adm_dwt_band_t *src, int scale, int w, int 
             accum_inner_d_256 = _mm256_add_epi64(accum_inner_d_256, d_cu);
         }
         __m128i h_r2 = _mm_add_epi64(_mm256_castsi256_si128(accum_inner_h_256), _mm256_extracti128_si256(accum_inner_h_256, 1));
-        uint64_t h_r1 = h_r2[0] + h_r2[1];
+        uint64_t h_r1 = mm_hadd_epi64(h_r2);
 
         __m128i d_r2 = _mm_add_epi64(_mm256_castsi256_si128(accum_inner_d_256), _mm256_extracti128_si256(accum_inner_d_256, 1));
-        uint64_t d_r1 = d_r2[0] + d_r2[1];
+        uint64_t d_r1 = mm_hadd_epi64(d_r2);
 
         __m128i v_r2 = _mm_add_epi64(_mm256_castsi256_si128(accum_inner_v_256), _mm256_extracti128_si256(accum_inner_v_256, 1));
-        uint64_t v_r1 = v_r2[0] + v_r2[1];
+        uint64_t v_r1 = mm_hadd_epi64(v_r2);
 
         for (int j = right_mod_4; j < right; ++j)
         {
