023d771d3261cb6873e07c1744110155624370ae
Kyle Swanson <kswanson@netflix.com>
2026-09-14 22:01:00 +0000

    feature/adm/arm64: fix dropped filter tap in adm_dwt2_8_neon column 0

--- libvmaf/src/feature/arm64/adm_neon.c
+++ libvmaf/src/feature/arm64/adm_neon.c
@@ -113,7 +113,7 @@ void adm_dwt2_8_neon(const uint8_t *src, const adm_dwt_band_t *dst,
         int32_t accum_h = add_shift_HP;
         int32_t accum_d = add_shift_HP;
 
-        for (int idx = 0; idx < 3; idx++)
+        for (int idx = 0; idx < 4; idx++)
         {
             int j_idx = ind_x[idx][0];
             int16_t s_lo = tmplo[j_idx];
